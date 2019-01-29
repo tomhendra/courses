f616331ce4dbc9a7508a03d41b5ca727d3957472
tomhendra <tom.hendra@outlook.com>
2019-01-29 17:57:06 +0100

    getSavedTodos changed to use ternary operator

--- todo-functions.js
+++ todo-functions.js
@@ -1,11 +1,7 @@
 // Fetch existing todos from local storage
 const getSavedTodos = () => {
     const todosJSON = localStorage.getItem('todos')
-    if (todosJSON !== null) {
-        return JSON.parse(todosJSON)
-    } else {
-        return []
-    }
+    return todosJSON !== null ? JSON.parse(todosJSON) : []
 }
 
 // Save todos to localStorage
