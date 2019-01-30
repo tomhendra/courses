fee7b14abf1dcfe9dfd6a7aa918d455fd00109f8
tomhendra <tom.hendra@outlook.com>
2019-01-30 16:37:13 +0100

    Guess handler
    - Added prototype method for Hangman obj to handle guess
    - if not already guessed: push to guessed letters array
    - if not already guessed & not in word: decrement remaining guesses

--- hangman.js
+++ hangman.js
@@ -1,9 +1,11 @@
+// Object constructor function
 const Hangman = function (word, remainingGuesses) {
     this.word = word.toLowerCase().split('')
     this.remainingGuesses = remainingGuesses
-    this.guessedLetters = ['e', 'b']
+    this.guessedLetters = []
 }
 
+// Method -- return result after guess
 Hangman.prototype.getPuzzle = function () {
     let puzzle = ''
     this.word.forEach ((letter) => {
@@ -16,7 +18,27 @@ Hangman.prototype.getPuzzle = function () {
     return puzzle
 }
 
-const game1 = new Hangman('beer', 4)
-const game2 = new Hangman('Bacon baguette', 6)
+// Method -- handle guess
+Hangman.prototype.makeGuess = function (guess) {
+    guess = guess.toLowerCase()
+    const isUnique = !this.guessedLetters.includes(guess)
+    const isBadGuess = !this.word.includes(guess)
+
+        if (isUnique) {
+            this.guessedLetters.push(guess)
+        }
+        if (isUnique && isBadGuess) {
+            this.remainingGuesses--
+        }
+    }
+
+// Testing, testing, 123
+const game1 = new Hangman('beer', 2)
+game1.makeGuess('b')
+game1.makeGuess('e')
+game1.makeGuess('z')
 console.log(game1.getPuzzle())
+
+const game2 = new Hangman('Bacon baguette', 4)
+game2.makeGuess('b')
 console.log(game2.getPuzzle())
