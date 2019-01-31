3bc8c13e69cf0646725355ff0f68e05b8797d387
tomhendra <tom.hendra@outlook.com>
2019-01-31 17:48:06 +0100

    Get puzzle data from JSON
    - requests.js: fetch JSON data for 'word' using callback model
    - getPuzzle: only fires callback once HTTP request data is good
    - app.js: callback function passed to requests.js

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
-const game1 = new Hangman('beer', 2)
-const game2 = new Hangman('Salamanca', 6)
+const game1 = new Hangman('Salamanca', 6)
 
 // Render puzzle
-puzzleEl.textContent = game2.puzzle
-guessesEl.textContent = game2.statusMessage
+puzzleEl.textContent = game1.puzzle
+guessesEl.textContent = game1.statusMessage
 
 window.addEventListener('keypress', (e) => {
     const guess = String.fromCharCode(e.charCode)
-    game2.makeGuess(guess)
-    puzzleEl.textContent = game2.puzzle
-    guessesEl.textContent = game2.statusMessage
+    game1.makeGuess(guess)
+    puzzleEl.textContent = game1.puzzle
+    guessesEl.textContent = game1.statusMessage
+})
+
+// Callback to get puzzle data via requests.js 
+getPuzzle((error, puzzle) => {
+    if (error) {
+        console.log(`Error: ${error}`)
+    } else {
+        console.log(puzzle)
+    }
 })
