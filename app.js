4d3431b49800214f88c1b1e5b05c14941f1650b8
tomhendra <tom.hendra@outlook.com>
2019-02-04 16:29:52 +0100

    Async/await refactor - requests.js refactored to use async - app.js refatored, calling getPuzzle async & seperate render - reset button added & EventListener

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
-const game1 = new Hangman('Salamanca', 6)
-
-// Render puzzle
-puzzleEl.textContent = game1.puzzle
-guessesEl.textContent = game1.statusMessage
+let game1
 
 window.addEventListener('keypress', (e) => {
     const guess = String.fromCharCode(e.charCode)
     game1.makeGuess(guess)
-    puzzleEl.textContent = game1.puzzle
-    guessesEl.textContent = game1.statusMessage
+    render()
 })
 
-// Promise to get puzzle data via requests.js 
-getPuzzle('3').then((puzzle) => {
-    console.log(puzzle)
-}).catch((err) => {
-    console.log(`Error: ${err}`)
-})
+const render = () => {
+    puzzleEl.textContent = game1.puzzle
+    guessesEl.textContent = game1.statusMessage
+}
+
+const startGame = async () => {
+    const puzzle = await getPuzzle('2')
+    game1 = new Hangman(puzzle, 5)
+    render()
+}
+
+document.querySelector('#reset').addEventListener('click', startGame)
+
+startGame()
--- requests.js
+++ requests.js
@@ -1,12 +1,11 @@
-// Promise to ensure HTTP request codes are good
-const getPuzzle = (wordCount) => {
-    return fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`).then((response) => {
-        if (response.status === 200) {
-            return response.json()
-        } else {
-            throw new Error('Unable to fetch puzzle')
-        }
-    }).then((data) => {
+// Async/await to retrieve puzzle from API
+const getPuzzle = async (wordCount) => {
+    const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+
+    if (response.status === 200) {
+        const data = await response.json()
         return data.puzzle
-    })
+    } else {
+        throw new Error('Unable to get puzzle')
+    }
 }
