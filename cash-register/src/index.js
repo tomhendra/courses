eda8327c36383f0878b8c9a3ac2c543cf2fa5714
tomhendra <tom.hendra@outlook.com>
2019-02-07 19:01:26 +0100

    Main functions created

--- cash-register/src/index.js
+++ cash-register/src/index.js
@@ -1,51 +1,85 @@
+/*********************************************************
+
+JavaScript Algorithms and Data Structures Projects: Cash Register 
+
+Design a cash register drawer function checkCashRegister() that accepts purchase price as the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid) as the third argument.
+
+cid is a 2D array listing available currency.
+
+The checkCashRegister() function should always return an object with a status key and a change key.
+
+Return {status: "INSUFFICIENT_FUNDS", change: []} if cash-in-drawer is less than the change due, or if you cannot return the exact change.
+
+Return {status: "CLOSED", change: [...]} with cash-in-drawer as the value for the key change if it is equal to the change due.
+
+Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins and bills, sorted in highest to lowest order, as the value of the change key. 
+
+********************************************************/
 
 const checkCashRegister = (price, cash, cid) => {
-    // Setup default change object
+    // Setup default change object to return
     const change = {
       status: '',
       change: []
     }
-    // Convert cid array to object
-    const cidObj = Object.fromEntries(cid)
-
-    // Get total value of cid
-    const cidTotal = Object.values(cidObj).reduce((a, b) => a + b).toFixed(2)
-
-    // Calculate change due
-    const changeDue = (cash - price)
-
-    
-
-    console.log(change)
+    // Setup currency object
+    const currencyObj = {
+        'ONE HUNDRED': 100,
+        TWENTY: 20,
+        TEN: 10,
+        FIVE: 5,
+        ONE: 1,
+        QUARTER: 0.25,
+        DIME: 0.1,
+        NICKEL: 0.05,
+        PENNY: 0.01
+    }
+    // Convert cid array to object & reorder highest to lowest
+    const cidObj = Object.fromEntries(cid.reverse())
     console.log(cidObj)
-    console.log(cidTotal)
-    console.log(changeDue)
+    // Get total value of cid
+    const getTotal = (obj) => Object.values(obj).reduce((a, b) => a + b).toFixed(2)
+    // Outputs array of key/value pairs to return as change.change value
+    const cidArr = (obj) => Object.entries(obj)
 
+    // Iterate over currency object and return new object with notes/coins
+    // Bonus: Deducts cid to reflect notes/coins taken out
+    const calculateChange = (currency) => {
+        let changeObj = {}
+        let changeAmount = (cash - price)
+        console.log(`Change to return: $${changeAmount}`)
+        Object.entries(currency).forEach(([key, value]) => {
+            if (changeAmount > currency[key]) {
+                let changeCount = 0
+                for (let i = 0; changeAmount >= currency[key] && cidObj[key] >= currency[key]; i++) {              
+                    changeAmount = changeAmount.toFixed(2)
+                    changeAmount -= currency[key]
+                    changeCount += currency[key]
+                    cidObj[key] -= currency[key]
+                }
+                changeObj[key] = changeCount
+            }
+        })
+        console.log(`Change amount: $${changeAmount}`)
+        console.log(cidObj)
+        console.log(changeObj)
+        return changeObj
+    }
+
+    calculateChange(currencyObj)
 
   }
+
+  // Test cases --->
   
-  checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+//   checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
 
+  checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) 
 
-// Design a cash register drawer function checkCashRegister() that accepts purchase price as the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid) as the third argument.
+//   checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
 
-// cid is a 2D array listing available currency.
+// checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
 
-// The checkCashRegister() function should always return an object with a status key and a change key.
+// checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
 
-// Return {status: "INSUFFICIENT_FUNDS", change: []} if cash-in-drawer is less than the change due, or if you cannot return the exact change.
-
-// Return {status: "CLOSED", change: [...]} with cash-in-drawer as the value for the key change if it is equal to the change due.
-
-// Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins and bills, sorted in highest to lowest order, as the value of the change key.
-
-// Currency Unit	Amount
-// Penny	$0.01 (PENNY)
-// Nickel	$0.05 (NICKEL)
-// Dime	$0.1 (DIME)
-// Quarter	$0.25 (QUARTER)
-// Dollar	$1 (DOLLAR)
-// Five Dollars	$5 (FIVE)
-// Ten Dollars	$10 (TEN)
-// Twenty Dollars	$20 (TWENTY)
-// One-hundred Dollars	$100 (ONE HUNDRED)
+// checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) 
