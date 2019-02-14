bc40912905248ee25d298c8c0d008cef5fed3fb3
tomhendra <tom.hendra@outlook.com>
2019-02-14 11:00:34 +0100

    Frequency counter pattern

--- problem-solving-patterns/src/index.js
+++ problem-solving-patterns/src/index.js
@@ -20,7 +20,7 @@
 // - usually O(n)
 
 // ----------------------------------------------------------------------------------------------------------
-// challenge --- write a function called same, which accepts 2 arrays. The function should return true if 
+// challenge 1 --- write a function called same, which accepts 2 arrays. The function should return true if 
 // every value in the array has it's corresponding value squared in the second array. The frequency of 
 // values must be the same.
 // ----------------------------------------------------------------------------------------------------------
@@ -62,8 +62,6 @@ const sameRefactored = (arr1, arr2) => {
     for (let val of arr2) {
         frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1        
     }
-    console.log(frequencyCounter1);
-    console.log(frequencyCounter2);
     for (let key in frequencyCounter1) {
         if (!(key ** 2 in frequencyCounter2)) {
             return false
@@ -76,3 +74,61 @@ const sameRefactored = (arr1, arr2) => {
 }
 
 sameRefactored([1,2,3,2,5], [9,1,4,4,11])
+
+// ----------------------------------------------------------------------------------------------------------
+// challenge 2 --- given two strings, write a function to determine if the second string is an anagram of 
+// the first. An anagram is word, phrase or name formed by rearranging the letters of another.
+// ----------------------------------------------------------------------------------------------------------
+// My solution (3 for loops)
+const validAnagram = (str1, str2) => {
+    if (str1.length !== str2.length) {
+        return false
+    }
+    let frequencyCounter1 = {}
+    let frequencyCounter2 = {}
+    for (let val of str1) {
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1
+    }
+    for (let val of str2) {
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1  
+    }
+    for (let key in frequencyCounter1) {
+        if (frequencyCounter1[key] !== frequencyCounter2[key]) {
+            return false
+        }
+    }
+    return true
+}
+console.log(validAnagram('','')) // true
+console.log(validAnagram('aaz', 'zza')) // false
+console.log(validAnagram('anagram', 'nagaram')) // true
+console.log(validAnagram('rat', 'car')) // false
+console.log(validAnagram('awesome', 'awsom')) // false
+
+// Tutor's solution (2 for loops)
+const validAnagram2 = (first, second) => {
+    if (first.length !== second.length) {
+      return false;
+    }
+  
+    const lookup = {};
+  
+    for (let i = 0; i < first.length; i++) {
+      let letter = first[i];
+      // if letter exists, increment, otherwise set to 1
+      lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+    }
+    console.log(lookup)
+  
+    for (let i = 0; i < second.length; i++) {
+      let letter = second[i];
+      // can't find letter or letter is zero then it's not an anagram
+      if (!lookup[letter]) {
+        return false;
+      } else {
+        lookup[letter] -= 1;
+      }
+    }
+  
+    return true;
+  }
