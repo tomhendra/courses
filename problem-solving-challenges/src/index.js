0b761f07e8ae2af5e85fdaa387e051349768a182
tomhendra <tom.hendra@outlook.com>
2019-02-18 15:17:42 +0100

    Challenge 3 completed

--- problem-solving-challenges/src/index.js
+++ problem-solving-challenges/src/index.js
@@ -2,6 +2,7 @@
 // challenge 1 --- write a function called sameFrequency. Given 2 positive integers, find out if the numbers
 // have the same frequency of digits.
 // ----------------------------------------------------------------------------------------------------------
+// frequency counter solution
 const sameFrequency = (num1, num2) => {
     const str1 = num1.toString()
     const str2 = num2.toString()
@@ -37,12 +38,13 @@ console.log(sameFrequency(22,222)) // false
 // challenge 2 --- write a function called areThereDuplicates which accepts a variable number of arguments,
 // and checks whether there are duplicates amongst the arguments passed in. 
 // ----------------------------------------------------------------------------------------------------------
+// Multiple pointers solution
 const areThereDuplicates = (...arr) => {
     arr.sort((a,b) => a > b)
     let start = 0;
     let next = 1;
     while (next < arr.length) {
-      if(args[start] === args[next]) {
+      if(arr[start] === arr[next]) {
           return true
       }
       start++
@@ -56,3 +58,31 @@ console.log('Frequency counter / multiple pointers challenge: areThereDuplicates
 console.log(areThereDuplicates(1,2,3)) // false
 console.log(areThereDuplicates(1,2,2)) // true
 console.log(areThereDuplicates('a', 'b', 'c', 'a')) // true
+
+// ----------------------------------------------------------------------------------------------------------
+// challenge 3 --- write a function called averagePair. Given a sorted array of integers nad a target average
+// determine if there is a pair of values in the array where the average of the pair equals the target 
+// average. There may be more than one pair that matches the average target.
+// ----------------------------------------------------------------------------------------------------------
+// multiple pointers solution
+const averagePair = (arr, target) => {
+    let left = 0
+    let right = arr.length - 1
+    while (left < right) {
+        let average = (arr[left] + arr[right]) / 2
+        if (average === target) {
+            return true
+        } else if (average > target) {
+            right--
+        } else {
+            left++
+        }
+    }
+    return false
+}
+// test cases
+console.log('Multiple pointers challenge: averagePair')
+console.log(averagePair([1,2,3],2.5)) // true
+console.log(averagePair([1,3,3,5,6,7,10,12,19],8)) // true
+console.log(averagePair([-1,0,3,4,5,6],4.1)) // false
+console.log(averagePair([],4)) // false
