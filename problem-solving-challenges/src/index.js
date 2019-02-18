9aa26a7716b6825f7f6373516a3966a496b5cfa7
tomhendra <tom.hendra@outlook.com>
2019-02-18 16:11:36 +0100

    Challenge 4 complete

--- problem-solving-challenges/src/index.js
+++ problem-solving-challenges/src/index.js
@@ -86,3 +86,24 @@ console.log(averagePair([1,2,3],2.5)) // true
 console.log(averagePair([1,3,3,5,6,7,10,12,19],8)) // true
 console.log(averagePair([-1,0,3,4,5,6],4.1)) // false
 console.log(averagePair([],4)) // false
+// ----------------------------------------------------------------------------------------------------------
+// challenge 4 --- write a function called isSubsequence which takes in 2 strings and checks whether the
+// characters from the first string form a subsequence of the characters in the second string.
+// ----------------------------------------------------------------------------------------------------------
+const isSubsequence = (str1, str2) => {
+    let i = 0;
+    let j = 0;
+    if (!str1) return true
+    while (j < str2.length) {
+      if (str2[j] === str1[i]) i++
+      if (i === str1.length) return true
+      j++
+    }
+    return false
+}
+// test cases
+console.log('Multiple pointers challenge: isSubsequence')
+console.log(isSubsequence('hello', 'hello world')) // true
+console.log(isSubsequence('sing', 'sting')) // true
+console.log(isSubsequence('abc', 'abracadabra')) // true
+console.log(isSubsequence('abc', 'acb')) // false
