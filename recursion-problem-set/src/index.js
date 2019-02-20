f75971ca6c4c8df10d50e0ad458ac9e6e68ed8d1
tomhendra <tom.hendra@outlook.com>
2019-02-20 12:10:48 +0100

    2nd challenge completed: factorial

--- recursion-problem-set/src/index.js
+++ recursion-problem-set/src/index.js
@@ -20,3 +20,20 @@ console.log(power(2,4)) // 16
 //     |    returns 2 * 2 // 4
 //     returns 2 * 4 // 8
 // returns 2 * 8 // 16
+
+// ----------------------------------------------------------------------------------------------------------
+// challenge 2 --- write a function called factorial which returns the factorial of that number. A factorial 
+// is he product of an integer and all the integers below it e.g. factorial four (!4) = 24 because 
+// 4 * 3 * 2 * 1 = 24. factorial zero (0!) is always 1.
+// ----------------------------------------------------------------------------------------------------------
+const factorial = (num) => {
+    if (num === 0) return 1
+    return num * factorial(num-1)
+}
+
+// test cases
+console.log('Challenge 2: factorial')
+console.log(factorial(1)) // 1
+console.log(factorial(2)) // 2
+console.log(factorial(4)) // 24
+console.log(factorial(7)) // 5040
