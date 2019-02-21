a150d345c3ff77f7a49a09cf5aab6ca1b65d8d78
tomhendra <tom.hendra@outlook.com>
2019-02-21 10:27:19 +0100

    5th challenge completed: fib

--- recursion-problem-set/src/index.js
+++ recursion-problem-set/src/index.js
@@ -63,3 +63,19 @@ const recursiveRange = (num) => {
 console.log('Challenge 4: recursiveRange')
 console.log(recursiveRange(6)) // 21
 console.log(recursiveRange(10)) // 55
+
+// ----------------------------------------------------------------------------------------------------------
+// challenge 5 --- write a recursive function called fib which accepts a number and returns the nth number
+// in the Fibonacci sequence. The Fibonacci sequence is the sequence of whole numbers 1,1,2,3,5,8,... which
+// starts with 1 & 1, and where every number thereafter is equal to the sum of the previous two numbers.
+// ----------------------------------------------------------------------------------------------------------
+const fib = (n) => {
+    if (n == 0 || n == 1)  return n;
+    return fib(n - 1) + fib(n - 2);
+}
+// test cases
+console.log('Challenge 4: fib')
+console.log(fib(4)) // 3
+console.log(fib(10)) // 55
+console.log(fib(28)) // 317811
+console.log(fib(35)) // 9227465
