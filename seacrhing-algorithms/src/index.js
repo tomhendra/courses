a0a2d50de1115b6356fdb65d9314e6290c9a9eea
tomhendra <tom.hendra@outlook.com>
2019-02-22 16:21:06 +0100

    Naive string search

--- seacrhing-algorithms/src/index.js
+++ seacrhing-algorithms/src/index.js
@@ -3,6 +3,7 @@
 // ==============
 // - most basic form of search
 // - the best we can do if data is unsorted!
+// - best case O(1) / worst case O(n)
 // - built in methods...
 // indexOf
 // includes
@@ -12,7 +13,7 @@
 // coding exercise 24 --- write a function called linearSearch which accepts an array & value, and returns
 // the index at which the value exists. If the value does not exist in the array, return -1.
 // ----------------------------------------------------------------------------------------------------------
-// - solution = O(n)
+// - solution
 const linearSearch = (arr, val) => {
     for (let i = 0; i < arr.length; i++)  {
         if (arr[i] === val) return i
@@ -29,10 +30,12 @@ console.log(linearSearch([1,2,3],3))
 // - ONLY works on sorted arrays
 // - rather than eliminating 1 element at a time, you can eliminate half the remaining elements at a time
 // - divide & conquer
+// - best case O(1) / worst & average case O(log n)
 // ----------------------------------------------------------------------------------------------------------
 // coding exercise 25 --- write a function called binarySearch which accepts an array & value, and returns
 // the index at which the value exists. If the value does not exist in the array, return -1.
 // ----------------------------------------------------------------------------------------------------------
+// - solution
 const binarySearch = (arr, val) => {
     let start = 0
     let end = arr.length -1
@@ -50,3 +53,20 @@ console.log(binarySearch([1,2,3,4,5],2)) // 1
 console.log(binarySearch([1,2,3,4,5],3)) // 2
 console.log(binarySearch([1,2,3,4,5],5)) // 4
 console.log(binarySearch([1,2,3,4,5],6)) // -1
+
+// ====================
+// NAIVE STRING SEARCH
+// ====================
+// - examples
+const stringSearch = (long, short) => {
+    let count = 0
+    for (let i = 0; i < long.length; i++) {
+        for (let j = 0; j < short.length; j++) {
+           if (short[j] !== long[i+j]) break
+           if (j === short.length - 1) count++
+        }
+    }
+    return count;
+}
+console.log('Short string search')
+console.log(stringSearch('hello harry potter', 'pot'))
