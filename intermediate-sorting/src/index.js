4bc4f8bd77d5115b502a27987275a6dbd621b302
tomhendra <tom.hendra@outlook.com>
2019-02-27 11:20:53 +0100

    Quick sort pivot

--- intermediate-sorting/src/index.js
+++ intermediate-sorting/src/index.js
@@ -74,3 +74,82 @@ console.log(mergeSort([42,7,2,1,67,24,12,3])) // [1,2,3,7,12,24,47,67]
 // ----------------------------------------------------------------------
 // merge sort       |   O(n log n)    |   O(n log n)    |      O(n)     |
 // ----------------------------------------------------------------------
+
+// ===========
+// QUICK SORT
+// ===========
+// - exploits the fact that arrays of 0 or 1 element are always sorted (like merge sort)
+// - works by selecting a single element (called "pivot") and finding the index where the pivot should end up in the sorted array
+// - once the pivot is positioned appropriately , quick sort can be applied either side of the pivot
+
+// ----------------------------------------------------------------------------------------------------------
+// challenge notes - helper function
+// - in order to implement quick sort, it is useful to first implement a function responsible for arranging 
+//   elements in an array on either side of the pivot
+// - given an array, this helper function should designate an element as as the pivot
+// - it should then rearrange elements in the array so that all values less than the pivot are moved to the
+//   left of the pivot, and all values greater than the pivot are moved to the right of the pivot 
+// - the order of elements on either side of the pivot doesn't matter!
+// - the helper should do this in place, that is, it should not create a new array
+// ----------------------------------------------------------------------------------------------------------
+// challenge notes - picking a pivot
+// - the runtime of quick sort depends in part on how the pivot is selected
+// - ideally, the pivot should be chosen so that it's roughly the median value in the data set you're sorting
+// - for simplicity, we'll always choose the pivot to be the first element
+// ----------------------------------------------------------------------------------------------------------
+// challenge pseudocode - pivot
+// - it will help to accept three arguments: an array, a start index and an end index (these can default to
+//   0 and the array length -1 respectively)
+// - grab the pivot from the start of the array
+// - store the current pivot index ina variable (this will keep track of where the pivot should end up)
+// - loop through the array from start until the end
+//     - if the pivot is greater than the current element, increment the pivot index variable and then swap
+//       the current element with the element at the pivot index
+// - swap the tarting element (i.e. the pivot) with the pivot index
+// - return the pivot index
+// ----------------------------------------------------------------------------------------------------------
+// first Version
+const pivotNaive = (arr, start=0, end=arr.length+1) => {
+    function swap(array, i, j) {
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  
+    var pivot = arr[start];
+    var swapIdx = start;
+  
+    for(var i = start + 1; i < arr.length; i++) {
+      if(pivot > arr[i]){
+        swapIdx++;
+        swap(arr,swapIdx,i);
+      }
+    }
+    swap(arr,start,swapIdx);
+    return swapIdx;
+  }
+  
+  // Version with ES2015 Syntax
+  const pivot = (arr, start = 0, end = arr.length - 1) => {
+    const swap = (arr, idx1, idx2) => {
+      [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+    }
+    // We are assuming the pivot is always the first element
+    let pivot = arr[start];
+    let swapIdx = start;
+  
+    for (let i = start + 1; i <= end; i++) {
+      if (pivot > arr[i]) {
+        swapIdx++;
+        swap(arr, swapIdx, i);
+        console.log(arr)
+      }
+    }
+    // Swap the pivot from the start the swapPoint
+    swap(arr, start, swapIdx);
+    console.log(arr)
+    return swapIdx;
+  }
+  // test cases
+  console.log('Quick sort - pivot')
+  console.log(pivot([4,8,2,1,5,7,6,3]))
