de3af5d93d6378cb2115d429766aa90994c1814f
tomhendra <tom.hendra@outlook.com>
2019-03-01 10:00:23 +0100

    Data structures introduction

---
+++ data-structures-intro/src/index.js
@@ -0,0 +1,6 @@
+// - Data structures are collections of values, the relationships among them, and the functions or operations that can be applied to the data
+// - Different data structures excel at different things.  Some are highly specialized, while others (like arrays) are more generally used.
+// - Working with map/location data? -- Use a graph!
+// - Need an ordered list with fast inserts/removals at the beginning and end? -- Use a linked list!
+// - Web scraping nested HTML? -- Use a tree!
+// - Need to write a scheduler? -- Use a binary heap!
