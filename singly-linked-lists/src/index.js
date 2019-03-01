18234e989ee7d163afa7313f99ccdead2cd09054
tomhendra <tom.hendra@outlook.com>
2019-03-01 12:52:44 +0100

    Intro to linked lists

---
+++ singly-linked-lists/src/index.js
@@ -0,0 +1,21 @@
+// =============
+// LINKED LISTS
+// =============
+// - Data structure that contains a head, tail and length property
+// - Linked lists consist of nodes, and each node has a value and a pointer to another node or null
+// - each node is connected one way directionally to the next node (apart from the last node which points to null)
+// - comparisons with arrays:
+//
+//     - lists...
+//     - do not have indexes
+//     - connected via nodes with a next pointer
+//     - random access is not allowed
+//
+//     - arrays...
+//     - indexed in order
+//     - insertion and deletion can be expensive
+//     - can quickly be accessed at a specific index
+
+// ----------------------------------------------------------------------------------------------------------
+// challenge pseudocode
+// ----------------------------------------------------------------------------------------------------------
