5535444e31c7d9ff46cf013a2aeae98c99c37156
tomhendra <tom.hendra@outlook.com>
2019-03-07 11:27:09 +0100

    Push method

--- double-linked-lists/src/index.js
+++ double-linked-lists/src/index.js
@@ -15,4 +15,35 @@ class DoublyLinkedList {
         this.tail = null;
         this.length = 0;
     }
+// ----------------------------------------------------------------------------------------------------------
+// challenge pseudocode -- push
+// - the function should accept a value
+// - create a new node using the value passed to the function
+// - if the is no head property on the list, set the head and tail to be the newly created node
+// - otherwise set the next property on the tail to be the new node
+// - set the previous property on the newly created node to be the old tail
+// - set the tail to be the newly created node
+// - increment the length by 1
+// - return the linked list
+// ----------------------------------------------------------------------------------------------------------
+    push(val) {
+        var newNode = new Node(val);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
+let list = new DoublyLinkedList()
+list.push('hello')
+list.push('goodbye')
+list.push('!!')
+list.push(':)')
+list.push('<3')
+console.log(list)
