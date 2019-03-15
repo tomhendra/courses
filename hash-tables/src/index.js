e856d76ea740e67c83b51d57af52a35e8b958dd4
tomhendra <tom.hendra@outlook.com>
2019-03-15 16:16:04 +0100

    HashTable class setup & set method

--- hash-tables/src/index.js
+++ hash-tables/src/index.js
@@ -37,10 +37,6 @@ function hashSimple (key, arrayLen) {
     return total;
   }
 
-  hashSimple("pink", 10) // 0
-  hashSimple("orange", 10) // 7
-  hashSimple("cyan", 10) // 3
-
 // - refining our hash functions - there are problems:
 //     - only hashes strings
 //     - not constant time - linear in key length
@@ -48,7 +44,7 @@ function hashSimple (key, arrayLen) {
 function hash (key, arrayLen) {
     let total = 0;
     let WEIRD_PRIME = 31;
-    for (let i = 0; i < Math.min(key.length, 100); i++) { // whichever is smaller between key length & first 100 chars will be what is looped
+    for (let i = 0; i < Math.min(key.length, 100); i++) { // whichever is smaller between key length & first 100 chars will be looped
       let char = key[i];
       let value = char.charCodeAt(0) - 96
       total = (total * WEIRD_PRIME + value) % arrayLen;
@@ -59,4 +55,59 @@ function hash (key, arrayLen) {
 //     - the prime number is useful in spreading out the keys more uniformly 
 //     - it's also helpful if the array being added to has a prime length
 //     - you don't need to know why (the maths is complicated!) 
-// - use an array length that is prime to avoid collisions!
+// - use an array length that is prime to help avoid collisions!
+
+// ========================
+// DEALING WITH COLLISIONS
+// ========================
+// - even with a large array and a great hash function, collisions are inevitable
+// - there are many strategies or dealing with collisions, but we'll focus on two:
+//     - separate chaining
+//     - linear probing
+//
+// - with separate chaining:
+//     - at each index in our array e store values using a more sophisticated data structure (e.g. an array or linked list)
+//     - this allows us to store multiple key-value pairs at the same index
+// - with linear probing:
+//     - when we find a collision, we search through the array to find the next empty slot
+//     - unlike with separate chaining, this allows us to store  single key-value at each index
+
+class HashTable {
+    constructor(size=4) {
+      this.keyMap = new Array(size);
+    }
+  
+    _hash(key) {
+      let total = 0;
+      let WEIRD_PRIME = 31;
+      for (let i = 0; i < Math.min(key.length, 100); i++) {
+        let char = key[i];
+        let value = char.charCodeAt(0) - 96
+        total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+      }
+      return total;
+    }
+// ----------------------------------------------------------------------------------------------------------
+// challenge pseudocode -- set
+// - accepts a key and a value
+// - hashes the key
+// - stores the key-value pair in the hash table array via separate chaining
+// ----------------------------------------------------------------------------------------------------------
+    set(key, value) {
+        let index = this._hash(key);
+        if (!this.keyMap[index]) {
+            this.keyMap[index] = [];
+        }
+        this.keyMap[index].push([key, value]);
+    }
+  }
+
+  let ht = new HashTable(17);
+  ht.set("maroon","#800000")
+  ht.set("yellow","#FFFF00")
+  ht.set("olive","#808000")
+  ht.set("salmon","#FA8072")
+  ht.set("lightcoral","#F08080")
+  ht.set("mediumvioletred","#C71585")
+  ht.set("plum","#DDA0DD")
+  console.log(ht);
