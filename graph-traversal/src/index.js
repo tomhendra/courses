c6e49864586444ae01d719fd1c3d1477241b6ba8
Tom Hendra <tom.hendra@outlook.com>
2019-03-18 12:58:15 +0100

    Graph traversal intro

---
+++ graph-traversal/src/index.js
@@ -0,0 +1,17 @@
+// ================
+// GRAPH TRAVERSAL
+// ================
+// - visiting, updating, checking each vertex in a graph
+// - many real world applications, and a very common interview question
+// - unlike a tree, a graph has no starting point, so it needs to be specified
+//
+// - graph traversal uses:
+//     - peer to peer networking
+//     - web crawlers
+//     - finding "closest"
+//     - matches / recommendations
+//     - shortest path problems
+//         - GPS navigation 
+//         - solving mazes
+//         - AI (shortest path to win a game)
+
