b4de762c44e285feffee0f31bb70b938bd43ca38
Tom Hendra <tom.hendra@outlook.com>
2019-03-20 16:28:03 +0100

    Dijkstra algorithm method

--- dijkstras-algorithm/src/index.js
+++ dijkstras-algorithm/src/index.js
@@ -14,12 +14,12 @@
 
 // - the approach can be broken down into four main steps:
 // - 1. Every time we look to visit a new node, we pick the node with the smallest known distance to visit first.
-// - 2. once we’ve moved to the node we’re going to visit, we look at each of its neighbors
-// - 3. for each neighboring node, we calculate the distance by summing the total edges that lead to the node we’re checking from 
+// - 2. once we’ve moved to the node we’re going to visit, we look at each of its neighbours
+// - 3. for each neighbouring node, we calculate the distance by summing the total edges that lead to the node we’re checking from 
 //      the starting node
 // - 4. if the new total distance to a node is less than the previous total, we store the new shorter distance for that node
 
-// min binary heap setup:
+// priority queue setup:
 class PriorityQueue {
     constructor() {
       this.values = [];
@@ -67,14 +67,70 @@ class WeightedGraph {
 //         - update the previous object to contain that vertex
 //         - enqueue the vertex with the total distance from the start node
 // ----------------------------------------------------------------------------------------------------------
+    Dijkstra(start, finish) {
+        const nodes = new PriorityQueue();
+        const distances = {};
+        const previous = {};
+        let path = [] //to return at end
+        let smallest;
+        //build up initial state
+        for (let vertex in this.adjacencyList) {
+            if (vertex === start) {
+                distances[vertex] = 0;
+                nodes.enqueue(vertex, 0);
+            } else {
+                distances[vertex] = Infinity;
+                nodes.enqueue(vertex, Infinity);
+            }
+            previous[vertex] = null;
+        }
+        // as long as there is something to visit
+        while (nodes.values.length){
+            smallest = nodes.dequeue().val;
+            if (smallest === finish) {
+                //WE ARE DONE
+                //BUILD UP PATH TO RETURN AT END
+                while (previous[smallest]) {
+                    path.push(smallest);
+                    smallest = previous[smallest];
+                }
+                break;
+            } 
+            if (smallest || distances[smallest] !== Infinity) {
+                for(let neighbour in this.adjacencyList[smallest]) {
+                    //find neighbouring node
+                    let nextNode = this.adjacencyList[smallest][neighbour];
+                    //calculate new distance to neighbouring node
+                    let candidate = distances[smallest] + nextNode.weight;
+                    let nextNeighbour = nextNode.node;
+                    if (candidate < distances[nextNeighbour]) {
+                        //updating new smallest distance to neighbour
+                        distances[nextNeighbour] = candidate;
+                        //updating previous - How we got to neighbour
+                        previous[nextNeighbour] = smallest;
+                        //enqueue in priority queue with new priority
+                        nodes.enqueue(nextNeighbour, candidate);
+                    }
+                }
+            }
+        }
+        return path.concat(smallest).reverse();     
+    }
 }
 
 const graph = new WeightedGraph()
-const queue = new PriorityQueue()
 graph.addVertex("A")
 graph.addVertex("B")
 graph.addVertex("C")
-graph.addEdge("A","B", 9)
-graph.addEdge("A","C", 5)
-graph.addEdge("B","C", 7)
-console.log(graph)
+graph.addVertex("D")
+graph.addVertex("E")
+graph.addVertex("F")
+graph.addEdge("A","B", 4)
+graph.addEdge("A","C", 2)
+graph.addEdge("B","E", 3)
+graph.addEdge("C","D", 2)
+graph.addEdge("C","F", 4)
+graph.addEdge("D","E", 3)
+graph.addEdge("D","F", 1)
+graph.addEdge("E","F", 1)
+console.log(graph.Dijkstra("A", "E"))
