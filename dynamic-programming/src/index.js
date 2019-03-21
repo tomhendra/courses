5038c4f8a20a2d44189ab9ce82bc2edb4df38ecb
Tom Hendra <tom.hendra@outlook.com>
2019-03-21 11:36:08 +0100

    Dynamic programming intro

--- dynamic-programming/src/index.js
+++ dynamic-programming/src/index.js
@@ -1,3 +1,16 @@
 // ====================
 // DYNAMIC PROGRAMMING
 // ====================
+// - a method for solving a complex problem by breaking it down into a collection of simpler subproblems, solving each of those subproblems just once, and storing those solutions
+// - most problems cannot be solved with dynamic programming - it's a small subset
+// - for the problems that can be solved with dynamic programming, it can make a huge difference in their performance
+// - it only works with:
+//     - optimal substructure
+//     - overlapping subproblems
+//
+// - overlapping subproblems 
+//     - a problem is said to have overlapping subproblems if it can be broken down into subproblems which are reused several times
+//     - e.g. fibonacci sequence: every number after the first two is the sum of the two preceding ones
+//
+// - optimal substructure
+//     - a problem is said to have optimal substructure if an optimal solution can be constructed from optimal solutions of its subproblems
