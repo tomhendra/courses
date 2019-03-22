68f4567626e017d3d9543dff4952fb815ca5b1b6
Tom Hendra <tom.hendra@outlook.com>
2019-03-22 09:33:14 +0100

    Exercise 40 complete & 39 setup

---
+++ 23-additional-coding-exercises/src/index.js
@@ -0,0 +1,72 @@
+// ============================================
+// ADDITIONAL CODING EXERCISED - THE WILD WEST
+// ============================================
+
+// ----------------------------------------------------------------------------------------------------------
+// coding exercises 35 - 38 are repeats of methods covered in SLL section
+// ----------------------------------------------------------------------------------------------------------
+// coding exercise 39 --- SLL rotate
+// this function should rotate all nodes in the list by some number passed in. e.g. if the list is 
+// 1 > 2 > 3 > 4 > 5 and you rotate by 2, the list should be modified to 3 > 4 > 5 > 1 > 2. the number passed
+// in can be any integer.
+// time complexity: O(n) where n is the list 
+// space complexity O(1)
+// ----------------------------------------------------------------------------------------------------------
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;   
+    }
+}
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val) {
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    rotate(val) {
+        
+    }
+}
+const list = new SinglyLinkedList
+list.push(1).push(2).push(3).push(4).push(5)
+console.log(list)
+
+// ----------------------------------------------------------------------------------------------------------
+// coding exercise 40 is a repeat of the set method covered in SLL section
+// ----------------------------------------------------------------------------------------------------------
+// coding exercise 41 --- divide & conquer - countZeroes
+// given an array of s and 0s which has all 1s followed by all 0s, write a function that returns all 0s in 
+// the array.
+// time complexity: O(log n)
+// ----------------------------------------------------------------------------------------------------------
+
+function countZeroes(array) {
+    let min = 0
+    let max = array.length - 1
+    let zeros = 0
+
+    while (min <= max) {
+        if (array[min] === 1) min++;
+        if (array[max] === 0) zeros++;
+        max--;
+    }
+    return zeros;
+}
+
+// console.log(countZeroes([1,1,1,1,0,0])) // 2
+// console.log(countZeroes([1,0,0,0,0])) // 4
+// console.log(countZeroes([0,0,0])) // 3
+// console.log(countZeroes([1,1,1,1])) // 0
