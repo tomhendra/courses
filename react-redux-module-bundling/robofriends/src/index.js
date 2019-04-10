3d44d56d1968e48db9333217a638fa2e8d841159
Tom Hendra <tom.hendra@outlook.com>
2019-04-10 18:23:41 +0200

    Added search box & heading - styled app - replaced robots.js with fetched online placegolder JSON

--- react-redux-module-bundling/robofriends/src/index.js
+++ react-redux-module-bundling/robofriends/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 import 'tachyons';
 import * as serviceWorker from './serviceWorker';
 
-import CardList from './CardList';
-import { robots } from './robots';
-
-ReactDOM.render(<CardList robots={robots}/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
