84f0ed7263d7c6583c001627ed82ae86384cbc92
Tom Hendra <tom.hendra@outlook.com>
2019-04-10 19:02:01 +0200

    Organised directories & code cleanup

---
+++ react-redux-module-bundling/robofriends/src/containers/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import './App.css';
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({ robots: users }))
+    }
+
+    onSearchChange = (event) => { // using arrow function for custom method ensures 'this' context refers to App rather than <input> of SearchBox, which is where it was called via the onChange property
+        this.setState({ searchfield: event.target.value })
+    }
+
+    render() {
+        const {robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+        })
+
+        return !robots.length ?
+        (
+            <div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                <h2 className='f2'>Loading...</h2>
+            </div>
+        ) 
+        : 
+        (
+            <div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+                    <CardList robots={filteredRobots}/>
+                </Scroll>
+            </div>
+        )
+    }
+}
+
+export default App;
