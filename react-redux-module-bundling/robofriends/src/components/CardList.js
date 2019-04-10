9ad9d4b8986b20404561163399ea165fc1bd3b00
Tom Hendra <tom.hendra@outlook.com>
2019-04-10 19:52:07 +0200

    Added error boundary

--- react-redux-module-bundling/robofriends/src/components/CardList.js
+++ react-redux-module-bundling/robofriends/src/components/CardList.js
@@ -2,6 +2,11 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({ robots }) => {
+
+    if (true) {
+        throw new Error('Meeeeeeeehhhhhh');
+    }
+
     return (
         <div>
             {
