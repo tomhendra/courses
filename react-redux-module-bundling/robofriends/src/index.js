8f880808b86ce2e58714043cb16cec8b5da52dfd
Tom Hendra <tom.hendra@outlook.com>
2019-04-11 16:48:32 +0200

    Added redux-logger & redux-thunk

--- react-redux-module-bundling/robofriends/src/index.js
+++ react-redux-module-bundling/robofriends/src/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import 'tachyons';
-import searchRobots from './reducers';
+import { searchRobots, requestRobots } from './reducers';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(searchRobots);
+const logger = createLogger();
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
