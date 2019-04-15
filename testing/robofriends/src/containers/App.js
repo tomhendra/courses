5e2f833ed87f352cedfb797acc20a0a866e215bb
Tom Hendra <tom.hendra@outlook.com>
2019-04-15 19:37:52 +0200

    Enzyme: testing connected components - seperated App.js (redux logic) from main render - moved main rendering logic to new file (MainPage.js) - implemented test examples for filterRobots() method

--- testing/robofriends/src/containers/App.js
+++ testing/robofriends/src/containers/App.js
@@ -1,14 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
-import ErrorBoundary from '../components/ErrorBoundary';
-import Header from '../components/Header';
-
-import './App.css';
-
 import { setSearchField, requestRobots } from '../actions';
+import MainPage from '../components/MainPage';
 
 const mapStateToProps = (state) => {
     return {
@@ -27,36 +20,8 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 class App extends Component {
-
-    componentDidMount() {
-        this.props.onRequestRobots()
-    }
-
     render() {
-        const { searchField, onSearchChange, robots, isPending } = this.props;
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchField.toLowerCase())
-        })
-
-        return isPending ?
-        (
-            <div className='tc'>
-                <Header />
-                <h2 className='f2'>Loading...</h2>
-            </div>
-        ) 
-        : 
-        (
-            <div className='tc'>
-                <Header />
-                <SearchBox searchChange={onSearchChange}/>
-                <Scroll>
-                    <ErrorBoundary>
-                        <CardList robots={filteredRobots}/>
-                    </ErrorBoundary>
-                </Scroll>
-            </div>
-        )
+       return <MainPage {...this.props} />
     }
 }
 
