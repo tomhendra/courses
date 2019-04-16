6d3d1d4c728d0295b44ebf6cdea6927b4467a8ac
Tom Hendra <tom.hendra@outlook.com>
2019-04-16 12:06:48 +0200

    Types in TypeScript

--- typescript/typescript.ts
+++ typescript/typescript.ts
@@ -1,6 +1,97 @@
-const sum = (a : number, b : number) => {
-    return a + b;
-}
- const answer = sum(4 , 5);
+//boolean
+let isCool: boolean = false;
 
- console.log(answer);
+//number
+let age: number = 56;
+
+//string
+let eyeColor: string = 'brown';
+let favouriteQuote: string = `I'm not old, I'm only ${age}`;
+
+//Array
+let pets: string[] = ['cat', 'mouse', 'dragon'];
+let pets2: Array<string> = ['pig', 'lion', 'dragon'];
+
+//Tuple
+let basket: [string, number];
+basket = ['basketball', 10];
+
+//Enum
+enum Size {Small = 1, Medium, Large}
+let sizeName: string = Size[2];
+alert(sizeName); // Displays 'Medium' as its value is 2 above
+
+//Any - BE CAREFUL!!!!!!!!!!
+let whatever: any = 'aaaaghhhhhh noooooo!';
+
+//void (nothing returned)
+let sing = (): void => console.log('Lalalala')
+
+//null and undefined
+let meh: undefined = undefined;
+let noo: null = null;
+
+//never
+// test 2 things: 
+// 1. function never returns
+// 2. cannot have a reachable endpoint
+let error = (): never => {
+  throw Error('blah!');
+}
+
+// Type Assertions:
+let ohhithere: any = "OH HI THERE";
+
+let strLength: number = (ohhithere as string).length;
+
+//Interface
+interface RobotArmy {
+  count: number,
+  type: string,
+  magic?: string // ? means this property may or may not be part of the object
+}
+// can also use type keyword. difference is interfaces create new name which can be used anywhere, types do not.
+type RobotArmy2 = {
+    count: number,
+    type: string,
+    magic?: string
+  }
+
+let fightRobotArmy = (robots: RobotArmy) =>{
+  console.log('FIGHT!');
+}
+let fightRobotArmy2 = (robots: {count: number, type: string, magic?: string}) =>{
+  console.log('FIGHT!');
+}
+
+//Function
+let fightRobotArmyF = (robots: RobotArmy): void =>{
+  console.log('FIGHT!');
+}
+let fightRobotArmy2F = (robots: {count: number, type: string, magic?: string}): void =>{
+  console.log('FIGHT!');
+}
+
+// *** Classes
+class Animal {
+    private sing: string; // private prevents access to property (default is public)
+    constructor(sound: string) {
+        this.sing = sound;
+    }
+    greet() {
+        return "Hello, " + this.sing;
+    }
+}
+
+let lion = new Animal("Lion");
+// lion.sing
+
+
+//In TypeScript, there are several places where type inference
+//is used to provide type information when there is no explicit
+//type annotation. For example, in this code
+let x = 3;
+// automatically detects x is a number.
+
+//Union Type
+let confused: string | number = 'hello'
