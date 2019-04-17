f14d7f68dd7011f432aafba9312713004f749e3d
Tom Hendra <tom.hendra@outlook.com>
2019-04-17 13:16:45 +0200

    Used Express to... - set cookie management and CSP header

--- security/security-server-exercise/server.js
+++ security/security-server-exercise/server.js
@@ -1,19 +1,28 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 const helmet = require('helmet')
 var winston = require('winston');
 
 const bodyParser = require('body-parser');
 const app = express()
+app.use(morgan('combined'))
 app.use(cors())
 app.use(helmet())
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  res.cookie('session', '1', { httpOnly: true }) // prevents cookies being accessed by client side scripting
+  res.cookie('session', '1', { secure: true }) // ensures cookies only sent over HTTPS connections
 
+  // set Content Security Policy headers...
+  res.set({
+    'Content-Security-Policy': "script-src 'self' 'https://apis.google.com'"
+  })
+  res.send('Hello World!')
+})
 app.post('/secret', (req, res) => {
   const { userInput } = req.body;
-  console.log(userInput);
   if (userInput) {
     winston.log('info', 'user input: ' + userInput);
     res.status(200).json('success');
