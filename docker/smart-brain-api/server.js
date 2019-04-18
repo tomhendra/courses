0e3b74485b07936beca2546280aa1d823f07c9da
Tom Hendra <tom.hendra@outlook.com>
2019-04-18 17:56:53 +0200

    Added PostgreSQL to Docker environment

--- docker/smart-brain-api/server.js
+++ docker/smart-brain-api/server.js
@@ -12,12 +12,7 @@ const image = require('./controllers/image');
 
 const db = knex({
   client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : '',
-    password : '',
-    database : 'smart-brain'
-  }
+  connection: process.env.POSTGRES_URI
 });
 
 const app = express();
