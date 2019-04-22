07063e6cbcb24919fc15f3821ee4cdc7e8009e2e
Tom Hendra <tom.hendra@outlook.com>
2019-04-22 18:48:57 +0200

    Fixed signout functionality and dropdown alignment

--- sessions-jwt/smart-brain/src/components/Profile/ProfileIcon.js
+++ sessions-jwt/smart-brain/src/components/Profile/ProfileIcon.js
@@ -31,12 +31,12 @@ class ProfileIcon extends React.Component {
                 >
                     <img src="http://tachyons.io/img/logo.jpg" className="br-100 ba h3 w3 dib" alt="avatar" />
                 </DropdownToggle>
-                    <DropdownMenu 
+                    <DropdownMenu   
                     className="b--transparent shadow-5" 
-                    style={{marginTop: "5px", backgroundColor: "rgba(255, 255, 255, 0.5)"}}
+                    style={{right: 0, left: "auto", backgroundColor: "rgba(255, 255, 255, 0.5)"}} 
                     >
                         <DropdownItem>View profile</DropdownItem>
-                        <DropdownItem>Signout</DropdownItem>
+                        <DropdownItem onClick={() => this.props.onRouteChange('signout')}>Sign Out</DropdownItem>
                     </DropdownMenu>
                 </Dropdown>
             </div>
