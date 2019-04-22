87eafed265f6ec471aad7d94e8bacf01afe5b1aa
Tom Hendra <tom.hendra@outlook.com>
2019-04-22 17:56:27 +0200

    Added profile icon & dropdown

--- sessions-jwt/smart-brain-api/postgres/seed/seed.sql
+++ sessions-jwt/smart-brain-api/postgres/seed/seed.sql
@@ -1,5 +1,5 @@
 BEGIN TRANSACTION;
 
-INSERT into users (name, email, entries, joined) values ('Jessie', 'jessie@gmail.com', 5, '2018-01-01');
-insert into login (hash, email) values ('$2a$10$WAK21U0LWl7C//jJ.DOB2uPP1DJQh7KUDgasdyQeGzkop2Pzl8W7u', 'jessie@gmail.com');
+INSERT into users (name, email, entries, joined) values ('tom', 'tom@gmail.com', 5, '2018-01-01');
+insert into login (hash, email) values ('$2a$10$WAK21U0LWl7C//jJ.DOB2uPP1DJQh7KUDgasdyQeGzkop2Pzl8W7u', 'tom@gmail.com');
 COMMIT;
