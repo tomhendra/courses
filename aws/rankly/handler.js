728f10c123bd1099ecce59822142f0abdbd73d06
Tom Hendra <tom.hendra@outlook.com>
2019-04-24 16:21:18 +0200

    Lambda 'rankly' function setup

---
+++ aws/rankly/handler.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const emojis = [
+  '🐣',  '🐥',  '🏅', '🎖',  '🥉', '🥈', '🥇', '🏆', '🚀'
+];
+
+module.exports.rank = async (event) => {
+  const rank = event.queryStringParameters.rank;
+  const rankEmoji = emojis[rank > emojis.length ? emojis.length - 1 : rank]
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: rankEmoji,
+    }, null, 2),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
