0602fbb20e133e6661da20b90ef1c735517f2ba5
Tom Hendra <tom.hendra@outlook.com>
2019-04-25 10:35:12 +0200

    Robofriends copied to CI/CD

--- typescript/robofriends/src/api/api.js
+++
@@ -1,3 +0,0 @@
-const apiCall = (link) => fetch(link).then(response => response.json());
-
-export default apiCall
--- typescript/robofriends/src/components/Card.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Card from './Card';
-
-it('to render card component', () => {
-    expect(shallow(<Card />)).toMatchSnapshot();
-})
