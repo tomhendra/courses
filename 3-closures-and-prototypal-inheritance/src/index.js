b42f9fe49b2a9fdbeaedb52f76551c47881ce301
Tom Hendra <tom.hendra@outlook.com>
2019-05-06 11:17:17 +0200

    Prototypes exercise

--- 3-closures-and-prototypal-inheritance/src/index.js
+++ 3-closures-and-prototypal-inheritance/src/index.js
@@ -224,3 +224,25 @@ typeof {}
 var human = {mortal: true}
 var socrates = Object.create(human);
 human.isPrototypeOf(socrates); // true
+
+//Exercise - extend the functionality of a built in object
+
+//#1
+//Date object => to have new method .lastYear() which shows you last year 'YYYY' format.
+
+Date.prototype.lastYear = function(){
+  return this.getFullYear() - 1;
+}//'1899'
+
+
+//#Bonus
+// Mofify .map() to print '🗺' at the end of each item.
+Array.prototype.map = function() {
+  let arr = [];
+  for (let i = 0; i < this.length; i++) {
+    arr.push((this[i] + '🗺'));
+  }
+  return arr;
+}
+console.log([1,2,3].map())
+//1🗺, 2🗺, 3🗺
