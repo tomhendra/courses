21613a94966b82e11c0d326d4aaa4efe89a1cfa9
Tom Hendra <tom.hendra@outlook.com>
2019-05-06 12:08:53 +0200

    Oop introduction

---
+++ 4-object-oriented-programming/src/index.js
@@ -0,0 +1,12 @@
+// ============================
+// OBJECT ORIENTED PROGRAMMING 
+// ============================
+// - In all programs there are two primary components: Data (in memory) & Behaviour (functions)
+// - Objects allow us to use prototypal inheritance
+// - OOP: Bringing together the data and behaviour in a single location, makes it easier to understand how programs work
+// - FP: Data & behaviour are distinctly different things and should be kept separate for clarity
+// - It's not about OOP vs FP... Using both paradigms in unison makes for beautiful programs! :)
+
+// ----------------------------------------------------------------------------------------------------------
+//
+// ----------------------------------------------------------------------------------------------------------
