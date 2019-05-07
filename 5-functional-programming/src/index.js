220efa3bd4ff12d3ea7a22706ac671b9a61911d4
Tom Hendra <tom.hendra@outlook.com>
2019-05-07 15:59:35 +0200

    Memoization

--- 5-functional-programming/src/index.js
+++ 5-functional-programming/src/index.js
@@ -226,6 +226,58 @@ multiplyBy5(20)
 // - currying expects one argument at a time, partial application expects remaining arguments on second call
 // ----------------------------------------------------------------------------------------------------------
 
-const multiply = (a, b, c) => a * b * c
-const partialMultiplyBy5 = multiply.bind(null, 5) // using bind as a way to pass an argument
+const multiplyAgain = (a, b, c) => a * b * c
+const partialMultiplyBy5 = multiplyAgain.bind(null, 5) // using bind as a way to pass an argument
 partialMultiplyBy5(10, 20)
+
+
+// ----------------------------------------------------------------------------------------------------------
+// Memoization
+// - a specific form of caching
+// ----------------------------------------------------------------------------------------------------------
+
+//learn to cache
+function addTo80(n) {
+    return n + 80;
+  }
+  
+  addTo80(5)
+  
+  let cache = {};
+  function memoizeAddTo80(n) {
+    if (n in cache) {
+      return cache[n];
+    } else {
+      console.log('long time');
+      const answer = n + 80;
+      cache[n] = answer;
+      return answer;
+    }
+  }
+  
+  // console.log(1, memoizeAddTo80(6))
+  // // console.log(cache)
+  // // console.log('-----------')
+  // console.log(2, memoizeAddTo80(6))
+  
+  // let's make that better with no global scope. This is closure in javaScript.
+  function memoizeClosureAddTo80(n) { 
+    let cache = {}; // closure
+    return function(n) {
+      if (n in cache) {
+        return cache[n];
+      } else {
+        console.log('long time');
+        const answer = n + 80;
+        cache[n] = answer;
+        return answer;
+      }
+    }
+  }
+  
+  const memoized = memoizeClosureAddTo80();
+  console.log(1, memoized(6))
+  // console.log(cache)
+  // console.log('-----------')
+  console.log(2, memoized(6))
+  
