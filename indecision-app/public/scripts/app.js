71e633a7c3ef225fa1169a60ca18bb0ce92b7e5b
Tom Hendra <tom.hendra@outlook.com>
2019-05-10 18:09:34 +0200

    Conditional rendering in JSX

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -3,9 +3,25 @@
 console.log('App.js is running!');
 var app = {
   title: 'Indecision App',
-  subtitle: 'Put your life in the hands of a computer' // JSX - JavaScript XML
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two'] // JSX - JavaScript XML
 
 };
-var template = React.createElement("div", null, React.createElement("h1", null, app.title), React.createElement("p", null, app.subtitle), React.createElement("ol", null, React.createElement("li", null, "Item one"), React.createElement("li", null, "Item two")));
+var template = React.createElement("div", null, React.createElement("h1", null, app.title), app.subtitle && React.createElement("p", null, app.subtitle), React.createElement("p", null, app.options.length > 0 ? "Here are your options" : "No options"), React.createElement("ol", null, React.createElement("li", null, "Item one"), React.createElement("li", null, "Item two")));
+var user = {
+  name: 'Maria',
+  age: 29,
+  location: 'Salamanca'
+};
+
+function getLocation(location) {
+  if (location) {
+    return React.createElement("p", null, "Location: ", location);
+  }
+} // logical && operator: if first condition is true second part of the expression evaluates 
+// ternary operator good for doing one of two things, && expression good for doing one thing or nothing
+
+
+var templateTwo = React.createElement("div", null, React.createElement("h1", null, user.name ? user.name : 'Anonymous'), user.age && user.age >= 18 && React.createElement("p", null, "Age: ", user.age), getLocation(user.location));
 var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,15 +1,17 @@
 console.log('App.js is running!');
 
-var app = {
+const app = {
   title: 'Indecision App',
-  subtitle: 'Put your life in the hands of a computer'
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two']
 }
 
 // JSX - JavaScript XML
-var template = (
+const template = (
   <div>
     <h1>{app.title}</h1> 
-    <p>{app.subtitle}</p>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
     <ol>
       <li>Item one</li>
       <li>Item two</li>
@@ -17,6 +19,28 @@ var template = (
   </div>
 );
 
-var appRoot = document.getElementById('app');
+const user = {
+  name: 'Maria',
+  age: 29,
+  location: 'Salamanca'
+}
+
+function getLocation(location) {
+  if (location) {
+    return <p>Location: {location}</p>;
+  }
+}
+
+// logical && operator: if first condition is true second part of the expression evaluates 
+// ternary operator good for doing one of two things, && expression good for doing one thing or nothing
+const templateTwo = (
+  <div>
+    <h1>{user.name ? user.name : 'Anonymous'}</h1>
+    {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+    {getLocation(user.location)}
+  </div>
+);
+
+const appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
