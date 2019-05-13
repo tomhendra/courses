519f2f3323a34b92d3f5025da0e93586b7590868
Tom Hendra <tom.hendra@outlook.com>
2019-05-13 16:35:59 +0200

    Events & atttributes

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,35 +1,32 @@
 "use strict";
 
-// arguments object - no longer bound with arrow functions
-var add = function add(a, b) {
-  // console.log(arguments);
-  return a + b;
+console.log('App.js is running!');
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two']
+};
+var template = React.createElement("div", null, React.createElement("h1", null, app.title), app.subtitle && React.createElement("p", null, app.subtitle), React.createElement("p", null, app.options.length > 0 ? 'Here are your options' : 'No options'), React.createElement("ol", null, React.createElement("li", null, "Item one"), React.createElement("li", null, "Item two")));
+var count = 0;
+
+var addOne = function addOne() {
+  console.log('addOne');
 };
 
-console.log(add(55, 1, 1001)); // this keyword - no longer bound
-
-var user = {
-  name: 'Andrew',
-  cities: ['Philadelphia', 'New York', 'Dublin'],
-  printPlacesLived: function printPlacesLived() {
-    var _this = this;
-
-    return this.cities.map(function (city) {
-      return _this.name + ' has lived in ' + city;
-    });
-  }
+var minusOne = function minusOne() {
+  console.log('minusOne');
 };
-console.log(user.printPlacesLived()); // Challenge area
 
-var multiplier = {
-  numbers: [10, 20, 30],
-  multiplyBy: 3,
-  multiply: function multiply() {
-    var _this2 = this;
-
-    return this.numbers.map(function (number) {
-      return number * _this2.multiplyBy;
-    });
-  }
+var reset = function reset() {
+  console.log('reset');
 };
-console.log(multiplier.multiply());
+
+var templateTwo = React.createElement("div", null, React.createElement("h1", null, "Count: ", count, " "), React.createElement("button", {
+  onClick: minusOne
+}, "-1"), React.createElement("button", {
+  onClick: addOne
+}, "+1"), React.createElement("button", {
+  onClick: reset
+}, "Reset"));
+var appRoot = document.getElementById('app');
+ReactDOM.render(templateTwo, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -5,6 +5,7 @@ const app = {
   subtitle: 'Put your life in the hands of a computer',
   options: ['One', 'Two']
 };
+
 const template = (
   <div>
     <h1>{app.title}</h1>
@@ -17,26 +18,31 @@ const template = (
   </div>
 );
 
-const user = {
-  name: 'Andrew',
-  age: 26,
-  location: 'Philadelphia'
+
+let count = 0;
+
+const addOne = () => {
+  console.log('addOne')
 };
 
-function getLocation(location) {
-  if (location) {
-    return <p>Location: {location}</p>;
-  }
-}
+const minusOne = () => {
+  console.log('minusOne')
+};
+
+const reset = () => {
+  console.log('reset')
+};
 
 const templateTwo = (
   <div>
-    <h1>{user.name ? user.name : 'Anonymous'}</h1>
-    {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
-    {getLocation(user.location)}
+    <h1>Count: {count} </h1>
+    <button onClick={minusOne}>-1</button>
+    <button onClick={addOne}>+1</button>
+    <button onClick={reset}>Reset</button>
+
   </div>
 );
 
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
