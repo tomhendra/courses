3fb44ec96ffa9e75613745ff7a66d6b6eea5efd9
Tom Hendra <tom.hendra@outlook.com>
2019-05-15 18:44:25 +0200

    Default props

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -12,7 +12,7 @@ class IndecisionApp extends React.Component {
     this.handlePick = this.handlePick.bind(this);
     this.handleAddOption = this.handleAddOption.bind(this);
     this.state = {
-      options: []
+      options: props.options
     }
   }
   handleDeleteOptions() {
@@ -41,14 +41,12 @@ class IndecisionApp extends React.Component {
     });
   }
   render() {
-    const title = 'Indecision';
-    const subTitle = 'Put your life in the hands of a computer';
+    const subtitle = 'Put your life in the hands of a computer';
 
     return (
       <div>
         <Header 
-          title={title} 
-          subTitle={subTitle}
+          subtitle={subtitle}
         />
         <Action 
           hasOptions={this.state.options.length > 0} 
@@ -66,13 +64,21 @@ class IndecisionApp extends React.Component {
   }
 }
 
+IndecisionApp.defaultProps = {
+  options: []
+}
+
 const Header = (props) => {
   return (
     <div>
       <h1>{props.title}</h1>
-      <h2>{props.subTitle}</h2>
+      {props.subtitle && <h2>{props.subtitle}</h2>}
     </div>
   )
+}
+
+Header.defaultProps = {
+  title: 'Indecision'
 }
 
 const Action = (props) => {
@@ -139,4 +145,4 @@ class AddOption extends React.Component {
   }
 }
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp options={['Leyma', 'Balabushka']} />, document.getElementById('app'));
--- indecision-app/src/playground/counter-example.js
+++ indecision-app/src/playground/counter-example.js
@@ -5,7 +5,7 @@ class Counter extends React.Component {
     this.handleMinusOne = this.handleMinusOne.bind(this);
     this.handleReset = this.handleReset.bind(this);
     this.state = {
-      count: 0
+      count: props.count
     };
   }
   handleAddOne() {
@@ -41,4 +41,8 @@ class Counter extends React.Component {
   }
 }
 
-ReactDOM.render(<Counter />, document.getElementById('app'));
+Counter.defaultProps = {
+  count: 0
+}
+
+ReactDOM.render(<Counter count={2} />, document.getElementById('app'));
