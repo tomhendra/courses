7757ec906fac4911f1d78a4d6e8eccef5a09a074
Tom Hendra <tom.hendra@outlook.com>
2019-05-15 15:02:32 +0200

    counter app state exercise

--- indecision-app/src/playground/build-it-visible.js
+++ indecision-app/src/playground/build-it-visible.js
@@ -1,58 +1,33 @@
-
-const app = {
-  title: 'Visibility Toggle',
-  toggle: false,
-  details: 'here are some details'
-};
-
-const showDetails = () => {
-  app.toggle = !app.toggle
-  render();
+class VisibilityToggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+    this.state = {
+      visibility: false
+    }
+  }
+  handleToggleVisibility() {
+    this.setState((prevState) => {
+      return {
+        visibility: !prevState.visibility
+      }
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Visibility Toggle</h1>
+        <button onClick={this.handleToggleVisibility}>
+          {this.state.visibility ? 'Hide details' : 'Show details'}
+        </button>
+        {this.state.visibility && (
+          <div>
+            <p>Hey. These are some details you can now see!</p>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
-const render = () => {
-  const jsx = (
-    <div>
-      <h1>{app.title}</h1>
-      <button onClick={showDetails}>
-        {app.toggle ? 'Hide details' : 'Show details'}
-      </button>
-      {app.toggle && (
-        <p>{app.details}</p>
-      )}
-    </div>
-  );
-
-  ReactDOM.render(jsx, document.getElementById('app'));
-}
-
-render();
-
-
-// teacher solution
-// let visibility = false;
-
-// const toggleVisibility = () => {
-//   visibility = !visibility;
-//   render();
-// };
-
-// const render = () => {
-//   const jsx = (
-//     <div>
-//       <h1>Visibility Toggle</h1>
-//       <button onClick={toggleVisibility}>
-//         {visibility ? 'Hide details' : 'Show details'}
-//       </button>
-//       {visibility && (
-//         <div>
-//           <p>Hey. These are some details you can now see!</p>
-//         </div>
-//       )}
-//     </div>
-//   );
-
-//   ReactDOM.render(jsx, document.getElementById('app'));
-// };
-
-// render();
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
