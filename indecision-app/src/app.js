f881ee185e5f5c9b223f1d7db8e14b1a92552076
Tom Hendra <tom.hendra@outlook.com>
2019-05-15 15:53:21 +0200

    Indecision state part 1

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -6,16 +6,41 @@
 // 5. start again at 3.
 
 class IndecisionApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+    this.handlePick = this.handlePick.bind(this);
+    this.state = {
+      options: ['Thing one', 'Thing two', 'Thing four']
+    }
+  }
+  handleDeleteOptions() {
+    this.setState(() => {
+      return {
+        options: []
+      }
+    })
+  }
+  handlePick() {
+    const randomNum = Math.floor(Math.random() * this.state.options.length);
+    const option = this.state.options[randomNum];
+    alert(option);
+  }
   render() {
     const title = 'Indecision';
     const subTitle = 'Put your life in the hands of a computer';
-    const options = ['Thing one', 'Thing two', 'Thing four'];
 
     return (
       <div>
         <Header title={title} subTitle={subTitle}/>
-        <Action />
-        <Options options={options} />
+        <Action 
+          hasOptions={this.state.options.length > 0} 
+          handlePick={this.handlePick}
+        />
+        <Options 
+          options={this.state.options} 
+          handleDeleteOptions={this.handleDeleteOptions}
+        />
         <AddOption />
       </div>
     )
@@ -34,13 +59,15 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
-  handlePick() {
-    alert('handlePick');
-  }
   render() {
     return (
       <div>
-        <button onClick={this.handlePick}>What should I do?</button>
+        <button 
+          onClick={this.props.handlePick}
+          disabled={!this.props.hasOptions}
+        >
+          What should I do?
+        </button>
       </div>
     )
   }
@@ -51,17 +78,10 @@ class Options extends React.Component {
   // constructor function for React components gets called with props object
   // must call super with props to ensure access to this.props
   // correct context is set when component is initialized rather than each time it renders
-  constructor(props) { 
-    super(props);
-    this.HandleRemoveAll = this.HandleRemoveAll.bind(this); 
-  }
-  HandleRemoveAll() {
-    console.log(this.props.options);
-  }
   render() {
     return (
       <div>
-        <button onClick={this.HandleRemoveAll}>Remove All</button>
+        <button onClick={this.props.handleDeleteOptions}>Remove All</button>
         {
           // when creating an array of JSX we have to define a key on each
           this.props.options.map((option) => <Option key={option} optionText={option} />)
