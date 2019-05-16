da5a55710c4fcb2b8cdf62accde7e2dcbf37f286
Tom Hendra <tom.hendra@outlook.com>
2019-05-16 17:37:41 +0200

    Finished componts separation

---
+++ indecision-app/src/components/Options.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Option from './Option';
+
+const Options = props => (
+  <div>
+    <button onClick={props.handleDeleteOptions}>Remove All</button>
+    {props.options.length === 0 && <p>Please add an option to get started!</p>}
+    {// when creating an array of JSX we have to define a key on each
+    props.options.map(option => (
+      <Option
+        key={option}
+        optionText={option}
+        handleDeleteOption={props.handleDeleteOption}
+      />
+    ))}
+  </div>
+);
+
+export default Options;
