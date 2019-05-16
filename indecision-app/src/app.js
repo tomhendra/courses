26697a07b0303fc956207a26373dbbc602f525e9
Tom Hendra <tom.hendra@outlook.com>
2019-05-16 15:44:07 +0200

    imports & exports in webpack

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,1 +1,10 @@
+import subtract, { square, add } from './utils';
+import isSenior, { isAdult, canDrink } from './person';
+
 console.log('app.js is running');
+console.log(square(4));
+console.log(add(10, 20));
+console.log(subtract(5, 3));
+console.log('isAdult', isAdult(15));
+console.log('canDrink', canDrink(22));
+console.log('isSenior', isSenior(70));
