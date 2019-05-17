e72ac07c284db2176b2204f3c7e11594647e6d91
Tom Hendra <tom.hendra@outlook.com>
2019-05-17 11:12:31 +0200

    setup scss

--- indecision-app/webpack.config.js
+++ indecision-app/webpack.config.js
@@ -19,6 +19,14 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', 
+          'css-loader',
+          'sass-loader'
+        ]
+      }
     ],
   },
   devServer: {
