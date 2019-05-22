a6e25a34c5ed446a24c47ce94ec1aded381bdb06
Tom Hendra <tom.hendra@outlook.com>
2019-05-22 18:23:31 +0200

    Edit expense

--- expensify-app/src/playground/redux-expensify.js
+++ expensify-app/src/playground/redux-expensify.js
@@ -24,12 +24,18 @@ const addExpense = (
 );
 
 // REMOVE_EXPENSE
-const removeExpense = ({ id } ={}) => ({ 
+const removeExpense = ({ id } = {}) => ({ 
   type: 'REMOVE_EXPENSE',
   id
 });
 
 // EDIT_EXPENSE
+const editExpense = (id, updates) => ({
+  type: 'EDIT_EXPENSE',
+  id,
+  updates
+});
+
 // SET_TEXT_FILTER
 // SORT_BY_DATE
 // SORT_BY_AMOUNT
@@ -45,6 +51,17 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
       return [...state, action.expense];
     case 'REMOVE_EXPENSE':
       return state.filter(({ id }) => id !== action.id);
+    case 'EDIT_EXPENSE':
+      return state.map((expense) => {
+        if (expense.id === action.id) {
+          return { // spread operator to merge current state with updates from action
+            ...expense,
+            ...action.updates
+          }
+        } else {
+          return expense;
+        }
+      })
     default:
       return state;
   }
@@ -82,6 +99,7 @@ store.subscribe(() => {
 const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 70000  }));
 const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300  }));
 store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
 
 const demoState = {
   expenses: [{
