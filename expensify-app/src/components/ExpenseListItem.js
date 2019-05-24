4c10e45fa29442d1e1b47418995af37f7ba3e110
Tom Hendra <tom.hendra@outlook.com>
2019-05-24 19:29:42 +0200

    ExpenseListItem filter connectted to Redux

--- expensify-app/src/components/ExpenseListItem.js
+++ expensify-app/src/components/ExpenseListItem.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
-const ExpenseListItem = ({ description, amount, createdAt }) => (
+const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
   <div>
     <h3>{description}</h3>
     <p>Amount: &euro;{amount} | Date created: {createdAt}</p>
-    <button>Remove</button>
+    <button onClick={() => dispatch(removeExpense({id}))}>Remove</button>
   </div>
 );
 
-export default ExpenseListItem;
+export default connect()(ExpenseListItem);
