13c40586d3cebc4837f73ad153d7c06ba01386dd
Tom Hendra <tom.hendra@outlook.com>
2019-05-27 19:30:30 +0200

    Added date range filter

--- expensify-app/src/components/ExpenseForm.js
+++ expensify-app/src/components/ExpenseForm.js
@@ -78,6 +78,7 @@ export default class ExpenseForm extends React.Component {
             onDateChange={this.onDateChange}
             focused={this.state.calendarFocused}
             onFocusChange={this.onFocusChange}
+            id={this.props.expense.id}
             numberOfMonths={1}
             isOutsideRange={() => false}
           />
