de3288a09d45105200164e447391820bb8e3ab05
Tom Hendra <tom.hendra@outlook.com>
2019-05-28 18:52:52 +0200

    Replaced react-test-renderer with Enzyme - configured Enzyme - added raf polyfill - added enzyme-to-json

--- expensify-app/src/tests/components/Header.test.js
+++ expensify-app/src/tests/components/Header.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
+import toJSON from 'enzyme-to-json';
 import Header from '../../components/Header';
 
 test('Should render Header correctly', () => {
-  const renderer = new ReactShallowRenderer;
-  renderer.render(<Header />);
-  expect(renderer.getRenderOutput()).toMatchSnapshot()
+  const wrapper = shallow(<Header />);
+  expect(toJSON(wrapper)).toMatchSnapshot();
 });
