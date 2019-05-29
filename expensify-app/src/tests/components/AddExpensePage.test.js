7a609a223d077fb04b981b4248860e1dca099f3c
Tom Hendra <tom.hendra@outlook.com>
2019-05-29 15:33:18 +0200

    AddEspensePage tests - used mapDispatchToProps

---
+++ expensify-app/src/tests/components/AddExpensePage.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { AddExpensePage } from '../../components/AddExpensePage';
+import expenses from '../fixtures/expenses';
+
+let addExpense, history, wrapper;
+
+// reuse spies & wrappers throughout test cases
+beforeEach(() => {
+  addExpense = jest.fn();
+  history = { push: jest.fn() };
+  wrapper = shallow(<AddExpensePage addExpense={addExpense} history={history} />);
+});
+
+test('Should render AddExpensePage correctly', () => {
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('Should handle addExpense', () => {
+  wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
+  expect(history.push).toHaveBeenLastCalledWith('/');
+  expect(addExpense).toHaveBeenLastCalledWith(expenses[1]);
+});
