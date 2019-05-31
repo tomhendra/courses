cfeb204e9c8a735111cd35a0512f94a2a8dec965
Tom Hendra <tom.hendra@outlook.com>
2019-05-31 18:32:27 +0200

    Setup formatting for date & currency

--- expensify-app/src/components/ExpenseListItem.js
+++ expensify-app/src/components/ExpenseListItem.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+// import numeral from 'numeral';
+
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'EUR',
+  minimumFractionDigits: 2
+});
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
   <div>
     <Link to={`/edit/${id}`}><h3>{description}</h3></Link>
-    <p>Amount: &euro;{amount} | Date created: {createdAt}</p>
+    <p>
+      {formatter.format(amount / 100)}
+      | 
+      {moment(createdAt).format('MMMM Do, YYYY')}
+    </p>
   </div>
 );
 
