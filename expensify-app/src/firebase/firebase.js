4aa524ee608e99272bd0936a075877b370b2c2e3
Tom Hendra <tom.hendra@outlook.com>
2019-06-03 18:48:20 +0200

    Firebase challenge - push expense data to firebase

--- expensify-app/src/firebase/firebase.js
+++ expensify-app/src/firebase/firebase.js
@@ -1,38 +1,72 @@
 import * as firebase from 'firebase';
 
 const config = {
-  ?????????
+  // ENVIRONMENT_VARIABLES_HERE
 };
 
 firebase.initializeApp(config);
 
 const database = firebase.database();
 
-// listen / subscribe to changes, gets called with callback function, returns function...
-const onValueChange = database.ref().on('value', (snapshot) => {
-  console.log(snapshot.val());
-}, (e) => {
-  console.log('Error with data fetching', e);
+// start afresh...
+database.ref().remove();
+
+// Setup "expenses" with three items (description, note, amount, createdAt)
+database.ref('expenses').push({
+  description: 'Rent',
+  note: '',
+  amount: 109500,
+  createdAt: 976123498763
 });
 
-setTimeout(() => {
-  database.ref('age').set(37);
-}, 3500);
+database.ref('expenses').push({
+  description: 'Phone bill',
+  note: '',
+  amount: 5900,
+  createdAt: 976123498763
+});
 
-// using off() to unsubscribe...
-setTimeout(() => {
-  database.ref().off('value', onValueChange);
-}, 7000);
+database.ref('expenses').push({
+  description: 'Food',
+  note: '',
+  amount: 1200,
+  createdAt: 976123498763
+});
 
-setTimeout(() => {
-  database.ref('age').set(39);
-}, 10500);
+// Firebase doesn't support arrays, so new objects need to be added using push()...
+// database.ref('notes').push({
+//   title: 'Course topics',
+//   body: 'React, Angular, GraphQL'
+// });
+
+// database.ref('notes/-LgT6zlOiWbR1p4JUZtI').remove();
+
+// listen / subscribe to changes, gets called with callback function, returns function...
+// const onValueChange = database.ref().on('value', (snapshot) => {
+//   console.log(snapshot.val());
+// }, (e) => {
+//   console.log('Error with data fetching', e);
+// });
+
+// setTimeout(() => {
+//   database.ref('age').set(37);
+// }, 3500);
+
+// // using off() to unsubscribe...
+// setTimeout(() => {
+//   database.ref().off('value', onValueChange);
+// }, 7000);
+
+// setTimeout(() => {
+//   database.ref('age').set(39);
+// }, 10500);
 
 // challenge...
 // database.ref().on('value', (snapshot) => {
 //   const val = snapshot.val();
 //   console.log(`${val.name} is a ${val.occupation.title} at ${val.occupation.company}`);
 // });
+
 
 // using once to fetch data from db, which returns a promise...
 // database.ref()
@@ -44,25 +78,6 @@ setTimeout(() => {
 //     console.log('Error fetching data: ' , e);
 //   });
 
-// Setting db entries....
-// database.ref().set({
-//   name: 'Tom Hendra',
-//   age: 36,
-//   stressLevel: 6,
-//   isSingle: false,
-//   occupation: {
-//     title: 'Student',
-//     company: 'FreeCodeCamp'
-//   },
-//   location: {
-//     city: 'Salamanca',
-//     country: 'Spain'
-//   }
-// }).then(() => {
-//   console.log('Data is saved');
-// }).catch((e) => {
-//   console.log('This failed!', e);
-// });
 
 // // Update must be called with object...
 // // Nested objects cannot be used with update unless using forward slash and quotes (using forward slash isn't valid JS)...
@@ -78,6 +93,7 @@ setTimeout(() => {
 // }).catch((e) => {
 //   console.log('Update failed!', e);
 // });
+
 
 // remove entries from db...
 // database.ref('isSingle')
