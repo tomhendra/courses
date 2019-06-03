0126c737e885d8f8d047651e86ea9033b2cf02c2
Tom Hendra <tom.hendra@outlook.com>
2019-06-03 20:09:03 +0200

    Added redux-thunk - notes regarding process flow - edied confirgureStore to allow for middleware - composeEnhancers for compatibility with Redux devtools

--- expensify-app/src/actions/expenses.js
+++ expensify-app/src/actions/expenses.js
@@ -1,5 +1,19 @@
 import uuid from 'uuid';
 
+// -- without firebase
+// component calls action generator
+// action generator returns object
+// component dispatches object to reducer
+// reducer updates redux store 
+// (reducer takes in app’s current state + action, and produces the altered state based on the contents of the action)
+
+// -- with firebase
+// component calls action generator
+// action generator returns FUNCTION 
+// component dispatches FUNCTION (with middleware)
+// function runs
+// (function has the ability to dispatch other actions and do whatever it wants) 
+
 // ADD_EXPENSE
 export const addExpense = (
   {
