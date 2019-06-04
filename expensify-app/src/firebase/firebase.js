52d7b83d99fae5f46bb069aab2ef75fa0b8b9269
Tom Hendra <tom.hendra@outlook.com>
2019-06-04 16:55:42 +0200

    Setup test firebase database - config cross-env to use single script in all OS envirements - config dotenv to handle environment variables - seperate gitignore added for this project instead of relying on parent

--- expensify-app/src/firebase/firebase.js
+++ expensify-app/src/firebase/firebase.js
@@ -1,9 +1,14 @@
 import * as firebase from 'firebase';
 
 const config = {
-  // ENVIRONMENT_VARIABLES_HERE
-
-};
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER,
+  appID: process.env.FIREBASE_APP_ID
+};  
 
 firebase.initializeApp(config);
 
