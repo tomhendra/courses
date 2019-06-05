7c40810a189625cbdc9792a230579d443fef297b
Tom Hendra <tom.hendra@outlook.com>
2019-06-05 19:56:32 +0200

    Router functionality for login / logout - installed standalone history package - used conditional logic to ensure correct page is loaded

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -3,7 +3,7 @@ require('offline-plugin/runtime').install();
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
 import { firebase } from './firebase/firebase';
@@ -13,23 +13,32 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
   </Provider>
 );
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+let hasRendered = false;
+const renderApp = () => {
+  if (!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById('app'));
+    hasRendered = true;
+  }
+};
 
-store.dispatch(startSetExpenses()).then(() => {
-  ReactDOM.render(jsx, document.getElementById('app'));
-});
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
-    console.log('Log in')
+    store.dispatch(startSetExpenses()).then(() => {
+      renderApp();
+      if (history.location.pathname === '/') {
+        history.push('/dashboard')
+      }
+    });
   } else {
-    console.log('Log out')
+    renderApp();
+    history.push('/')
   }
 });
