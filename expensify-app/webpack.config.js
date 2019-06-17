43670fa5544d9f7fca02d87754ea1782baa0746e
Tom Hendra <tom.hendra@outlook.com>
2019-06-17 15:53:39 +0200

    More tests for firebase auth

--- expensify-app/webpack.config.js
+++ expensify-app/webpack.config.js
@@ -3,19 +3,9 @@ const common = require('./webpack/webpack.common');
 
 const envs = {
   development: 'dev',
-  test: 'test',
   production: 'prod',
 };
 
 let env = envs[process.env.NODE_ENV || 'development'];
-
-if (env === 'dev' || env === 'prod') {
-  require('dotenv').config({ path: '.env/.env.dev' });
-} else if (env === 'test') {
-  require('dotenv').config({ path: '.env/.env.test' });
-  env = 'dev'
-} 
-
-// require either webpack.dev or webpack.prod
 const envConfig = require(`./webpack/webpack.${env}.js`);
 module.exports = merge(common, envConfig);
