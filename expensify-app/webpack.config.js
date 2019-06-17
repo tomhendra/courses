da0a7b2fe6c5ea382cd3f4b4c6e335de2f882739
Tom Hendra <tom.hendra@outlook.com>
2019-06-17 15:27:04 +0200

    Logic change for firebase auth in production

--- expensify-app/webpack.config.js
+++ expensify-app/webpack.config.js
@@ -3,16 +3,19 @@ const common = require('./webpack/webpack.common');
 
 const envs = {
   development: 'dev',
+  test: 'test',
   production: 'prod',
-  test: 'test'
 };
 
 let env = envs[process.env.NODE_ENV || 'development'];
 
-if (env !== 'prod') {
-  require('dotenv').config({ path: `.env/.env.${env}` });
-  env = 'dev';
+if (env === 'dev' || env === 'prod') {
+  require('dotenv').config({ path: '.env/.env.dev' });
+} else if (env === 'test') {
+  require('dotenv').config({ path: '.env/.env.test' });
+  env = 'dev'
 } 
 
+// require either webpack.dev or webpack.prod
 const envConfig = require(`./webpack/webpack.${env}.js`);
 module.exports = merge(common, envConfig);
