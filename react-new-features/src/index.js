291f41a9840973d3309d3cb166b6b3e519ae8180
Tom Hendra <tom.hendra@outlook.com>
2019-06-19 20:19:38 +0200

    useState hook

--- react-new-features/src/index.js
+++ react-new-features/src/index.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<div>My new content</div>, document.getElementById('root'));
+// useState built-in hook can manipulate state without having to use class based components
+// returns an array of 2 things:
+// 1. current state 
+// 2. a function to change the state
+
+const App = (props) => {  
+
+  let [count, setCount] = useState(props.count)
+
+  return (
+    <div>
+      <p>The current count is {count}</p>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button onClick={() => setCount(count - 1)}>-1</button>
+      <button onClick={() => setCount(props.count)}>Reset</button>
+    </div>
+  );
+}
+
+App.defaultProps = {
+  count: 0
+}
+
+ReactDOM.render(<App count={2} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
