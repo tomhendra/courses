2ddbb15026deac4e77346a896fab7421c6b6db6e
Tom Hendra <tom.hendra@outlook.com>
2019-06-22 20:04:23 +0200

    More component separation - to finish AddNoteForm later...

--- react-new-features/src/components/NoteApp.js
+++ react-new-features/src/components/NoteApp.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useReducer } from 'react';
 import notesReducer from '../reducers/notes';
-import Note from './Note';
+import NoteList from './NoteList';
+import AddNoteForm from './AddNoteForm';
 
 const NoteApp = () => {
   const [notes, dispatch] = useReducer(notesReducer, [])
@@ -42,19 +43,9 @@ const NoteApp = () => {
   return (
     <div>
       <h1>Notes</h1>
-      {notes.map(note => (
-        <Note
-          key={note.title}
-          note={note}
-          removeNote={removeNote}
-        />
-      ))}
+      <NoteList notes={notes} removeNote={removeNote} />
       <p>Add note</p>
-      <form onSubmit={addNote}>
-        <input value={title} onChange={(e) => setTitle(e.target.value)} />
-        <textarea value={body} onChange={(e) => setBody(e.target.value)}></textarea>
-        <button>Add note</button>
-      </form>
+      <AddNoteForm />
     </div>
   )
 }
---
+++ react-new-features/src/components/NoteList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Note from './Note';
+
+const NoteList = ({ notes, removeNote }) => {
+  return notes.map(note => (
+  <Note
+    key={note.title}
+    note={note}
+    removeNote={removeNote}
+  />
+))}
+
+export { NoteList as default }
