48e6cabe41ac50f256cf184c55d74573e01046b8
Thomas Hendra <tom.hendra@outlook.com>
2019-07-01 17:19:37 +0200

    Conversion to JSX - added eslint extensions for React

--- complete-intro-to-react-v5/adopt-me/src/App.js
+++ complete-intro-to-react-v5/adopt-me/src/App.js
@@ -3,24 +3,33 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese"
-    }),
-    React.createElement(Pet, {
-      name: "Polo",
-      animal: "Dog",
-      breed: "Basset Hound"
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Mixed"
-    })
-  ]);
+  // return React.createElement("div", { id: "something-important" }, [
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   React.createElement(Pet, {
+  //     name: "Luna",
+  //     animal: "Dog",
+  //     breed: "Havanese"
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Polo",
+  //     animal: "Dog",
+  //     breed: "Basset Hound"
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Doink",
+  //     animal: "Cat",
+  //     breed: "Mixed"
+  //   })
+  // ]);
+
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <Pet name={"Luna"} animal={"Dog"} breed={"Havanese"} />
+      <Pet name={"Polo"} animal={"Dog"} breed={"Basset Hound"} />
+      <Pet name={"Doink"} animal={"Cat"} breed={"Mixed"} />
+    </div>
+  );
 };
 
 render(React.createElement(App), document.getElementById("root"));
--- complete-intro-to-react-v5/adopt-me/src/Pet.js
+++ complete-intro-to-react-v5/adopt-me/src/Pet.js
@@ -1,9 +1,11 @@
 import React from "react";
+
 export default function Pet({ name, animal, breed }) {
-  // createElement accepts 3 args: type of element, attributes & children (which can be an array)
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
 }
