9f1f19c91939ee53546bfed984ff0691e361b0da
Thomas Hendra <tom.hendra@outlook.com>
2019-07-01 18:07:45 +0200

    React hooks - intro - setup eslint

--- complete-intro-to-react-v5/adopt-me/src/App.js
+++ complete-intro-to-react-v5/adopt-me/src/App.js
@@ -1,35 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  // return React.createElement("div", { id: "something-important" }, [
-  //   React.createElement("h1", {}, "Adopt Me!"),
-  //   React.createElement(Pet, {
-  //     name: "Luna",
-  //     animal: "Dog",
-  //     breed: "Havanese"
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Polo",
-  //     animal: "Dog",
-  //     breed: "Basset Hound"
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Doink",
-  //     animal: "Cat",
-  //     breed: "Mixed"
-  //   })
-  // ]);
-
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name={"Luna"} animal={"Dog"} breed={"Havanese"} />
-      <Pet name={"Polo"} animal={"Dog"} breed={"Basset Hound"} />
-      <Pet name={"Doink"} animal={"Cat"} breed={"Mixed"} />
+      <SearchParams />
     </div>
   );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
