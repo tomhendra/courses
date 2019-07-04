1dfdad19496a5f93cfa82bf94183a5b2116f7cab
Thomas Hendra <tom.hendra@outlook.com>
2019-07-04 18:45:52 +0200

    Testing

--- intermediate-react-v2/adopt-me-testing/__mocks__/@frontendmasters/pet.js
+++ intermediate-react-v2/adopt-me-testing/__mocks__/@frontendmasters/pet.js
@@ -5,13 +5,15 @@ import { act } from "@testing-library/react";
 const breeds = [
   { name: "Bichon Frise" },
   { name: "Bolognese" },
+  { name: "Bolonka" },
   { name: "Coton de Tulear" },
   { name: "Havanese" },
+  { name: "Lowchen" },
   { name: "Maltese" }
 ];
 
 const doggos = JSON.parse(
-  readFileSync(path.join(__dirname, "res.json")).toString()
+  readFileSync(path.join(__dirname, "/res.json")).toString()
 );
 
 export const ANIMALS = ["dog", "cat", "bird"];
@@ -21,12 +23,20 @@ export const _dogs = doggos.animals;
 const mock = {
   breeds: jest.fn(() => {
     return {
-      then: callback => act(() => callback({ breeds }))
+      then: callback =>
+        act(() => {
+          callback({
+            breeds
+          });
+        })
     };
   }),
   animals: jest.fn(() => {
     return {
-      then: callback => act(() => callback(doggos))
+      then: callback =>
+        act(() => {
+          callback(doggos);
+        })
     };
   })
 };
---
+++ intermediate-react-v2/adopt-me-testing/jest.config.js
@@ -0,0 +1,8 @@
+// jest.config.js
+module.exports = {
+  setupFilesAfterEnv: [
+    "@testing-library/react/cleanup-after-each"
+    // ... other setup files ...
+  ]
+  // ... other options ...
+};
