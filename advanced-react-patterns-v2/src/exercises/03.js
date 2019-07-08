1fc413d00ec4f055973ac53ea8ce46c3885c6bf2
Thomas Hendra <tom.hendra@outlook.com>
2019-07-08 12:18:14 +0200

    Flexible comound components

--- advanced-react-patterns-v2/src/exercises/03.js
+++ advanced-react-patterns-v2/src/exercises/03.js
@@ -44,22 +44,43 @@ import {Switch} from '../switch'
 //   (newlines are ok, like in the above example)
 
 // 🐨 create a ToggleContext with React.createContext here
-
+const ToggleContext = React.createContext({
+  on: false,
+  toggle: () => {},
+})
 class Toggle extends React.Component {
   // 🐨 each of these compound components will need to be changed to use
   // ToggleContext.Consumer and rather than getting `on` and `toggle`
   // from props, it'll get it from the ToggleContext.Consumer value.
-  static On = ({on, children}) => (on ? children : null)
-  static Off = ({on, children}) => (on ? null : children)
-  static Button = ({on, toggle, ...props}) => (
-    <Switch on={on} onClick={toggle} {...props} />
+  static On = ({children}) => (
+    <ToggleContext.Consumer>
+      {contextValue => (contextValue.on ? children : null)}
+    </ToggleContext.Consumer>
   )
-  state = {on: false}
+  static Off = ({on, children}) => (
+    <ToggleContext.Consumer>
+      {contextValue => (contextValue.on ? null : children)}
+    </ToggleContext.Consumer>
+  )
+  static Button = props => (
+    <ToggleContext.Consumer>
+      {contextValue => (
+        <Switch
+          on={contextValue.on}
+          onClick={contextValue.toggle}
+          {...props}
+        />
+      )}
+    </ToggleContext.Consumer>
+  )
   toggle = () =>
     this.setState(
       ({on}) => ({on: !on}),
       () => this.props.onToggle(this.state.on),
     )
+  // state declaration needs to be moved below initialisation
+  // this.toggle = function in state, which is weird, but apparently the best way to handle
+  state = {on: false, toggle: this.toggle}
   render() {
     // Because this.props.children is _immediate_ children only, we need
     // to 🐨 remove this map function and render our context provider with
@@ -67,11 +88,15 @@ class Toggle extends React.Component {
     // expose the `on` state and `toggle` method as properties in the context
     // value (the value prop).
 
-    return React.Children.map(this.props.children, child =>
-      React.cloneElement(child, {
-        on: this.state.on,
-        toggle: this.toggle,
-      }),
+    return (
+      <ToggleContext.Provider
+        // any time the value changes the provider will re-render itself as well as all the consumers
+        // so we pass this.state which is an object which only changes when something actually does change
+        // but to provide the toggle this needs to be added to state as above
+        value={this.state}
+      >
+        {this.props.children}
+      </ToggleContext.Provider>
     )
   }
 }
