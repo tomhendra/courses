c5ca7884519fd421ff8c64d99f385fc235092b40
Thomas Hendra <tom.hendra@outlook.com>
2019-07-09 17:27:05 +0200

    Control props

--- advanced-react-patterns-v2/src/__tests__/10.js
+++ advanced-react-patterns-v2/src/__tests__/10.js
@@ -5,8 +5,8 @@ import {
 } from 'react-dom/test-utils'
 import chalk from 'chalk'
 import {fireEvent, renderToggle} from '../../test/utils'
-import Usage, {Toggle} from '../exercises-final/10'
-// import Usage, {Toggle} from '../exercises/10'
+// import Usage, {Toggle} from '../exercises-final/10'
+import Usage, {Toggle} from '../exercises/10'
 
 const findToggleInstances = rootInstance =>
   findAllInRenderedTree(rootInstance, c =>
@@ -71,8 +71,8 @@ test('toggle can still be uncontrolled', () => {
 /*
 http://ws.kcd.im/?ws=react%20patterns&e=10-primer&em=tom.hendra%40outlook.com
 */
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+test('I submitted my elaboration and feedback', () => {
+  const submitted = true // change this when you've submitted!
   expect(submitted).toBe(true)
 })
 ////////////////////////////////
