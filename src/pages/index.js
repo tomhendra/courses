80dd3dca9e407ba9b4e1798924cbebc6990e33ee
Thomas Hendra <tom.hendra@outlook.com>
2019-07-29 19:44:27 +0200

    Added hero component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import Hero from '../components/hero';
 import PostPreview from '../components/post-preview';
 import usePosts from '../hooks/use-posts';
 
 export default () => {
   const posts = usePosts();
   return (
-    <Layout>
-      <h1>Home</h1>
-      <p>Hi Frontend Masters!</p>
-      <Link to="/about/">Learn about me &rarr;</Link>
-
-      <h2>Read my blog</h2>
-      {posts.map(post => (
-        <PostPreview key={post.slug} post={post} />
-      ))}
-    </Layout>
+    <>
+      <Hero />
+      <Layout>
+        <h2>Read my blog</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+      </Layout>
+    </>
   );
 };
