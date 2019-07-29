f46325aad6541e42e77fd6e287ff513703392339
Thomas Hendra <tom.hendra@outlook.com>
2019-07-29 11:31:34 +0200

    Added blog previews to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import PostPreview from '../components/post-preview';
 import usePosts from '../hooks/use-posts';
 
 export default () => {
@@ -13,7 +14,7 @@ export default () => {
 
       <h2>Read my blog</h2>
       {posts.map(post => (
-        <pre>{JSON.stringify(post, null, 2)}</pre>
+        <PostPreview key={post.slug} post={post} />
       ))}
     </Layout>
   );
