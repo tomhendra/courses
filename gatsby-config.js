ef0aa0ff514f6f148ab10f08db38ec8e29c6f027
Thomas Hendra <tom.hendra@outlook.com>
2019-07-31 17:09:06 +0200

    Added Webpack bundle analysis

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,5 +48,14 @@ module.exports = {
         username: 'tomhendra',
       },
     },
+    {
+      resolve: 'gatsby-plugin-webpack-bundle-analyzer',
+      options: {
+        production: true,
+        disable: !process.env.ANALYZE_BUNDLE_SIZE,
+        generateStatsFile: true,
+        analyzerMode: 'static',
+      },
+    },
   ],
 };
