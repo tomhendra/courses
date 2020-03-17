5063c3838a414f32aa4e2891a23da4d3d470811b
Thomas Hendra <tom.hendra@outlook.com>
2020-03-17 12:29:32 +0100

    Section 13: Let's Build a Web Framework: Initial refector

--- udemy--typescript-complete-developers-guide/13-lets-build-a-web-framework/src/index.ts
+++ udemy--typescript-complete-developers-guide/13-lets-build-a-web-framework/src/index.ts
@@ -2,4 +2,8 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Hubert Cufflebum', age: 65 });
 
-user.save();
+user.events.on('change', () => {
+  console.log('Change!!');
+});
+
+user.events.trigger('change');
---
+++ udemy--typescript-complete-developers-guide/13-lets-build-a-web-framework/src/models/Attributes.ts
@@ -0,0 +1,15 @@
+import { UserProps } from './User';
+
+export class Attributes<T> {
+  constructor(private data: T) {}
+
+  // generic constraint on method
+  // type of K an only ever be one of the keys of T
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key];
+  }
+
+  set(update: T): void {
+    Object.assign(this.data, update);
+  }
+}
