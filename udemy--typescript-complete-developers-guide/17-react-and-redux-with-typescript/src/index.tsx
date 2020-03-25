6711b32c7c711c9e380e8ec334a310fc33a73038
Thomas Hendra <tom.hendra@outlook.com>
2020-03-25 11:15:14 +0100

    Section 17: React and Redux with Typescript

---
+++ udemy--typescript-complete-developers-guide/17-react-and-redux-with-typescript/src/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { App } from './components/App';
+import { reducers } from './reducers';
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
+
+/* Functional component
+const App = (props: AppProps): JSX.Element => {
+  return <div>{props.color}</div>;
+};
+*/
