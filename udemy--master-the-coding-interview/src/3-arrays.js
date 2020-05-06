cd03fa2a36431e6b4f35ea888725cd78c764332f
Thomas Hendra <tom.hendra@outlook.com>
2020-05-06 11:39:54 +0200

    Merge sorted arrays

--- udemy--master-the-coding-interview/src/3-arrays.js
+++ udemy--master-the-coding-interview/src/3-arrays.js
@@ -89,3 +89,43 @@ const reverse3 = (str) => [...str].reverse().join('');
 console.log(reverse('moT si eman ym ,iH'));
 console.log(reverse('moT si eman ym ,iH'));
 console.log(reverse3('moT si eman ym ,iH'));
+
+// merge sorted arrays
+function mergeSortedArrays(array1, array2) {
+  const mergedArray = [];
+  let array1Item = array1[0];
+  let array2Item = array2[0];
+  let i = 1;
+  let j = 1;
+
+  //We should actually move these 2 if statements to line 2 so that
+  // we do the checks before we do assignments in line 3 and 4!
+  if (array1.length === 0) {
+    return array2;
+  }
+  if (array2.length === 0) {
+    return array1;
+  }
+
+  while (array1Item || array2Item) {
+    if (array2Item === undefined || array1Item < array2Item) {
+      mergedArray.push(array1Item);
+      array1Item = array1[i];
+      i++;
+    } else {
+      mergedArray.push(array2Item);
+      array2Item = array2[j];
+      j++;
+    }
+  }
+  return mergedArray;
+}
+
+// better readability
+function mergeSortedArrays2(arr1, arr2) {
+  // check inputs
+  return [...arr1, ...arr2].sort((a, b) => a - b);
+}
+
+console.log(mergeSortedArrays([0, 3, 4, 31], [3, 4, 6, 30]));
+console.log(mergeSortedArrays2([0, 3, 4, 31], [3, 4, 6, 30]));
