4c31c0d82b452d159702334750d1d727b21ee696
Thomas Hendra <tom.hendra@outlook.com>
2020-05-08 17:47:30 +0200

    Find the first recurring number

--- udemy--master-the-coding-interview/src/4-hash-tables.js
+++ udemy--master-the-coding-interview/src/4-hash-tables.js
@@ -62,9 +62,58 @@ class HashTable {
 
 const myHashTable = new HashTable(50);
 myHashTable.set('grapes', 10000);
-myHashTable.set('apples', 9);
+myHashTable.set('apples', 9000);
 myHashTable.set('oranges', 900);
 myHashTable.keys();
 console.log(myHashTable.data);
 console.log(myHashTable.get('grapes'));
 console.log(myHashTable.get('apples'));
+
+// Google Question
+// Given an array = [2,5,1,2,3,5,1,2,4]:
+// It should return 2
+
+// Given an array = [2,1,1,2,3,5,1,2,4]:
+// It should return 1
+
+// Given an array = [2,3,4,5]:
+// It should return undefined
+
+// Naive approach -- O(n^2)
+function firstRecurringCharacter(input) {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+      if (input[i] === input[j]) {
+        return input[i];
+      }
+    }
+  }
+  return undefined;
+}
+
+// Bonus... What if we had this:
+// [2,5,5,2,3,5,1,2,4]
+// return 5 because the pairs are before 2,2
+function firstRecurringCharacter2(input) {
+  let map = {};
+  for (let i = 0; i < input.length; i++) {
+    if (map[input[i]] !== undefined) {
+      return input[i];
+    } else {
+      map[input[i]] = i;
+    }
+  }
+  return undefined;
+}
+
+// more readable
+function firstRecurringCharacter3(input) {
+  const set = new Set();
+  return input.find((element) => {
+    if (set.has(element)) return true;
+    set.add(element);
+  });
+}
+
+console.log(firstRecurringCharacter3([2, 5, 1, 2, 3, 5, 1, 2, 4]));
+console.log(firstRecurringCharacter3([2, 5, 5, 2, 3, 5, 1, 2, 4]));
