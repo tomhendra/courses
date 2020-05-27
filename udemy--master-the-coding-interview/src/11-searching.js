dfc36ddb38099ecb371eeb5399393441b70da2c6
Thomas Hendra <tom.hendra@outlook.com>
2020-05-27 09:30:51 +0200

    BFS vs DFS exercise

--- udemy--master-the-coding-interview/src/11-searching.js
+++ udemy--master-the-coding-interview/src/11-searching.js
@@ -14,3 +14,23 @@ beasts.find(function (item) {
 });
 
 beasts.includes('Godzilla');
+
+// Exercise -- interview questions
+
+// If you know a solution is not far from the root of the tree:
+// BFS
+
+// If the tree is very deep and solutions are rare:
+// BFS (DFS will take a long time)
+
+// If the tree is very wide:
+// DFS (BFS will need too much memory, storing all the nodes)
+
+// If solutions are frequent but located deep in the tree:
+// DFS
+
+// Determining whether a path exists between two nodes:
+// BFS
+
+// Finding the shortest path:
+// BFS
