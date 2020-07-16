2540413e96e0c665d47cf067a24c3ca5560f483b
Thomas Hendra <tom.hendra@outlook.com>
2020-07-16 12:40:07 +0200

    Getting Started with JS: Programming Primer exercise

---
+++ 14-fem-beginner/2-getting-started-with-javascript-v2/exercises/primer/ex.js
@@ -0,0 +1,25 @@
+// TODO: define addFavoriteBook(..) function
+function addFavoriteBook(bookName) {
+  if (!bookName.includes('Great')) {
+    favoriteBooks.push(bookName);
+  }
+}
+
+// TODO: define printFavoriteBooks() function
+function printFavoriteBooks() {
+  console.log(`Favorite Books: ${favoriteBooks.length}.`);
+  for (let book of favoriteBooks) {
+    console.log(book);
+  }
+}
+
+var favoriteBooks = [];
+
+addFavoriteBook('A Song of Ice and Fire');
+addFavoriteBook('The Great Gatsby');
+addFavoriteBook('Crime & Punishment');
+addFavoriteBook('Great Expectations');
+addFavoriteBook("You Don't Know JS");
+
+// TODO: print out favorite books
+printFavoriteBooks();
