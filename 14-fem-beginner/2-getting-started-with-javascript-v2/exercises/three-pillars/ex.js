a1f1680fbf569e1149cd02f9d52d225dce86a088
Thomas Hendra <tom.hendra@outlook.com>
2020-07-20 17:37:54 +0200

    Getting Started with JS: Three pillars exercise

--- 14-fem-beginner/2-getting-started-with-javascript-v2/exercises/three-pillars/ex.js
+++ 14-fem-beginner/2-getting-started-with-javascript-v2/exercises/three-pillars/ex.js
@@ -1,43 +1,46 @@
 class Bookshelf {
-	constructor() {
-		this.favoriteBooks = [];
-	}
+  constructor() {
+    this.favoriteBooks = [];
+  }
 
-	// TODO: define methods `addFavoriteBook(..)`
-	// and `printFavoriteBooks()`
+  addFavoriteBook(bookName) {
+    if (!bookName.includes('Great')) {
+      this.favoriteBooks.push(bookName);
+    }
+  }
+
+  printFavoriteBooks() {
+    console.log(`Favorite Books: ${String(this.favoriteBooks.length)}`);
+    for (let bookName of this.favoriteBooks) {
+      console.log(bookName);
+    }
+  }
 }
 
-function addFavoriteBook(bookName) {
-	if (!bookName.includes("Great")) {
-		favoriteBooks.push(bookName);
-	}
+function loadBooks(bookShelf) {
+  fakeAjax(BOOK_API, function onBooks(bookNames) {
+    for (let bookName of bookNames) {
+      bookShelf.addFavoriteBook(bookName);
+    }
+    bookShelf.printFavoriteBooks();
+  });
 }
 
-function printFavoriteBooks() {
-	console.log(`Favorite Books: ${favoriteBooks.length}`);
-	for (let bookName of favoriteBooks) {
-		console.log(bookName);
-	}
-}
-
-function loadBooks( /* .. */ ) {
-	// TODO: call fakeAjax( .. );
-}
-
-var BOOK_API = "https://some.url/api";
-
+var BOOK_API = 'https://some.url/api';
+var myBooks = new Bookshelf();
+loadBooks(myBooks);
 
 // ***********************
 
 // NOTE: don't modify this function at all
-function fakeAjax(url,cb) {
-	setTimeout(function fakeLoadingDelay(){
-		cb([
-			"A Song of Ice and Fire",
-			"The Great Gatsby",
-			"Crime & Punishment",
-			"Great Expectations",
-			"You Don't Know JS"
-		]);
-	},500);
+function fakeAjax(url, cb) {
+  setTimeout(function fakeLoadingDelay() {
+    cb([
+      'A Song of Ice and Fire',
+      'The Great Gatsby',
+      'Crime & Punishment',
+      'Great Expectations',
+      "You Don't Know JS",
+    ]);
+  }, 500);
 }
