a730f8bf26c668f4226439a1d9d73dd64c03996c
Thomas Hendra <tom.hendra@outlook.com>
2020-09-17 19:06:50 +0200

    Complete Intro to React v5: Context

---
+++ 15-fem-professional/4-complete-intro-to-react-v5/adopt-me/context/src/Details.js
@@ -0,0 +1,63 @@
+import React from "react";
+import ThemeContext from "./ThemeContext";
+import pet from "@frontendmasters/pet";
+import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
+
+class Details extends React.Component {
+  state = { loading: true };
+
+  componentDidMount() {
+    // throw new Error("loool");
+    pet
+      .animal(+this.props.id)
+      .then(({ animal }) => {
+        this.setState({
+          name: animal.name,
+          animal: animal.type,
+          location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+          description: animal.description,
+          media: animal.photos,
+          breed: animal.breeds.primary,
+          loading: false,
+        });
+      })
+      .catch((err) => this.setState({ error: err }));
+  }
+  render() {
+    if (this.state.loading) {
+      return <h1>loading... </h1>;
+    }
+
+    const { animal, breed, location, description, media, name } = this.state;
+
+    return (
+      <div className="details">
+        <Carousel media={media} />
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} — ${breed} — ${location}`}</h2>
+          <ThemeContext.Consumer>
+            {([theme]) => (
+              <button
+                style={{ backgroundColor: theme }}
+                onClick={this.toggleModal}
+              >
+                Adopt {name}
+              </button>
+            )}
+          </ThemeContext.Consumer>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
