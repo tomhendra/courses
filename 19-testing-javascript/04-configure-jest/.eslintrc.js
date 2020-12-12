349b6385a916e6b346639cdbdca5032f18af4758
Tom Hendra <tom.hendra@outlook.com>
2020-12-12 12:13:12 +0100

    Configuring Jest: Support Importing CSS files

--- 19-testing-javascript/04-configure-jest/.eslintrc.js
+++ 19-testing-javascript/04-configure-jest/.eslintrc.js
@@ -1,21 +1,31 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
   extends: [
-    'kentcdodds',
-    'kentcdodds/import',
-    'kentcdodds/jest',
-    'kentcdodds/react',
+    "kentcdodds",
+    "kentcdodds/import",
+    "kentcdodds/jest",
+    "kentcdodds/react"
   ],
   rules: {
     // https://github.com/benmosher/eslint-plugin-import/issues/1446
-    'import/named': 'off',
+    "import/named": "off"
   },
-  settings: {'import/resolver': 'node'},
+  settings: { "import/resolver": "node" },
   overrides: [
     {
-      files: ['**/src/**'],
-      settings: {'import/resolver': 'webpack'},
+      files: ["**/src/**"],
+      settings: { "import/resolver": "webpack" }
     },
-  ],
-}
+    {
+      files: ["**/__tests__/**"],
+      settings: {
+        "import/resolver": {
+          jest: {
+            jestConfigFile: path.join(__dirname, "./jest.config.js")
+          }
+        }
+      }
+    }
+  ]
+};
