e24d889ad630909e33a36a174fc2fa2cbef6299c
Tom Hendra <tom.hendra@outlook.com>
2021-01-12 12:42:10 +0100

    Epic React: useState: Tic Tac Toe Extra credit

--- 03-algorithms-and-data-structures/01-big-o-notation/src/index.js
+++ 03-algorithms-and-data-structures/01-big-o-notation/src/index.js
@@ -1,17 +1,17 @@
 // add all numbers from 1 to n
 const addUpTo = (n) => {
-    let total = 0
-    for (let i = 1; i <= n; i++) {
-        total += n
-    }
-    return total
-}
+  let total = 0;
+  for (let i = 1; i <= n; i++) {
+    total += n;
+  }
+  return total;
+};
 
-const addUpTo2 = (n) => n * (n + 1) / 2 // n(1)
-const t1 = performance.now()
-console.log(addUpTo2(1000000000))
-const t2 = performance.now()
-console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds`)
+const addUpTo2 = (n) => (n * (n + 1)) / 2; // n(1)
+const t1 = performance.now();
+console.log(addUpTo2(1000000000));
+const t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds`);
 
 // The problem with time...
 // Different machines will record different times
@@ -19,33 +19,32 @@ console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds`)
 // For fast algorithms, speed measurements may not be enough
 // That's where Big O Notation comes in......
 
-
 // ------------------ TIME COMPLEXITY ------------------
 // Number of operations is (eventually) bounded by a multiple of n
 
 // O(n) -- simple operations grow proportionately to the value of n
 const countUpAndDown = (n) => {
-    console.log('Going up!')
-    for (let i = 0; i < n; i++) {
-        console.log(i)
-    }
-    console.log('At the top.\nGoing down!...')
-    for (let j = n - 1; j >= 0; j--) {
-        console.log(j)
-    }
-    console.log('Back down, bye!')
-}
-countUpAndDown(10)
+  console.log("Going up!");
+  for (let i = 0; i < n; i++) {
+    console.log(i);
+  }
+  console.log("At the top.\nGoing down!...");
+  for (let j = n - 1; j >= 0; j--) {
+    console.log(j);
+  }
+  console.log("Back down, bye!");
+};
+countUpAndDown(10);
 
 // O(n2) -- when O(n) operation nested inside of O(n) operation i.e. O(n * n) = (quadratic)
 const printAllPairs = (n) => {
-    for (let i = 0; i < n; i++) {
-        for (let j = 0; j < n; j++) {
-            console.log(i, j)
-        }
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      console.log(i, j);
     }
-}
-printAllPairs(10)
+  }
+};
+printAllPairs(10);
 
 // Simplifying Big O
 // Some rules of thumb (that don't ALWAYS work but are a good starting point)....
@@ -53,7 +52,6 @@ printAllPairs(10)
 // 2. variable assignment is constant
 // 3. accessing elements in an array (by index) or object (by key) is constant
 // 4. in a loop , the complexity is the length of the loop * the complexity of whatever happens inside the loop
-
 
 // ------------------ AUXILIARY SPACE COMPLEXITY ------------------
 // space in memory required by the algorithm, not including space taken up by inputs
@@ -65,21 +63,21 @@ printAllPairs(10)
 
 // O(1) space! -- only two variables taking constant space
 const sum = (arr) => {
-    let total = 0
-    for (let i = 0; i < arr.length; i++) {
-        total += arr[i]
-    }
-    return total
-}
-console.log(sum([1, 2, 3, 4, 5]))
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total;
+};
+console.log(sum([1, 2, 3, 4, 5]));
 
 // O(n) space! -- length of newArr is directly proportionate to input arr (n)
 const double = (arr) => {
-    let newArr = []
-    for (let i = 0; i < arr.length; i++) {
-        newArr.push(2 * arr[i])
-    }
-}
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(2 * arr[i]);
+  }
+};
 
 // ------------------ LOGARITHMS ------------------
 // the inverse of exponents (to power of)
--- 20-epic-react/react-hooks/src/exercise/04.js
+++ 20-epic-react/react-hooks/src/exercise/04.js
@@ -1,33 +1,14 @@
 // useState: tic tac toe
-// http://localhost:3000/isolated/exercise/04.js
+// 💯 add game history feature
+// http://localhost:3000/isolated/final/04.extra-3.js
 
 import * as React from 'react'
 import {useLocalStorageState} from '../utils'
 
-function Board() {
-  const [squares, setSquares] = useLocalStorageState(
-    'squares',
-    Array(9).fill(null),
-  )
-
-  const nextValue = calculateNextValue(squares)
-  const winner = calculateWinner(squares)
-  const status = calculateStatus(winner, squares, nextValue)
-
-  function selectSquare(square) {
-    if (winner || squares[square]) return
-    const squaresCopy = [...squares]
-    squaresCopy[square] = nextValue
-    setSquares(squaresCopy)
-  }
-
-  function restart() {
-    setSquares(Array(9).fill(null))
-  }
-
+function Board({squares, onClick}) {
   function renderSquare(i) {
     return (
-      <button className="square" onClick={() => selectSquare(i)}>
+      <button className="square" onClick={() => onClick(i)}>
         {squares[i]}
       </button>
     )
@@ -35,7 +16,6 @@ function Board() {
 
   return (
     <div>
-      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -51,24 +31,64 @@ function Board() {
         {renderSquare(7)}
         {renderSquare(8)}
       </div>
-      <button className="restart" onClick={restart}>
-        restart
-      </button>
     </div>
   )
 }
 
 function Game() {
+  const [history, setHistory] = useLocalStorageState('history', [
+    Array(9).fill(null),
+  ])
+  const [currentStep, setCurrentStep] = useLocalStorageState('step', 0)
+
+  const currentSquares = history[currentStep]
+  const winner = calculateWinner(currentSquares)
+  const nextValue = calculateNextValue(currentSquares)
+  const status = calculateStatus(winner, currentSquares, nextValue)
+
+  function selectSquare(square) {
+    if (winner || currentSquares[square]) return
+    const newHistory = history.slice(0, currentStep + 1)
+    const squares = [...currentSquares]
+
+    squares[square] = nextValue
+    setHistory([...newHistory, squares])
+    setCurrentStep(newHistory.length)
+  }
+
+  function restart() {
+    setHistory([Array(9).fill(null)])
+    setCurrentStep(0)
+  }
+
+  const moves = history.map((stepSquares, step) => {
+    const desc = step ? `Go to move #${step}` : 'Go to game start'
+    const isCurrentStep = step === currentStep
+    return (
+      <li key={step}>
+        <button disabled={isCurrentStep} onClick={() => setCurrentStep(step)}>
+          {desc} {isCurrentStep ? '(current)' : null}
+        </button>
+      </li>
+    )
+  })
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board onClick={selectSquare} squares={currentSquares} />
+        <button className="restart" onClick={restart}>
+          restart
+        </button>
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{moves}</ol>
       </div>
     </div>
   )
 }
 
-// eslint-disable-next-line no-unused-vars
 function calculateStatus(winner, squares, nextValue) {
   return winner
     ? `Winner: ${winner}`
@@ -77,14 +97,12 @@ function calculateStatus(winner, squares, nextValue) {
     : `Next player: ${nextValue}`
 }
 
-// eslint-disable-next-line no-unused-vars
 function calculateNextValue(squares) {
   const xSquaresCount = squares.filter(r => r === 'X').length
   const oSquaresCount = squares.filter(r => r === 'O').length
   return oSquaresCount === xSquaresCount ? 'X' : 'O'
 }
 
-// eslint-disable-next-line no-unused-vars
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
