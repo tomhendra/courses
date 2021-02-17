0e33ebce90137d11b58554bb093ff1b3f9f9f823
Tom Hendra <tom.hendra@outlook.com>
2021-02-17 09:34:41 +0100

    Epic React: Form testing

--- 20-epic-react/testing-react-apps/src/__tests__/exercise/04.js
+++ 20-epic-react/testing-react-apps/src/__tests__/exercise/04.js
@@ -5,25 +5,31 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
+import {build, fake} from '@jackfranklin/test-data-bot'
+
+const buildLoginForm = build({
+  fields: {
+    username: fake(f => f.internet.userName()),
+    password: fake(f => f.internet.password()),
+  },
+})
 
 test('submitting the form calls onSubmit with username and password', () => {
-  let submittedData
-  const handleSubmit = data => (submittedData = data)
+  // Hint: screen.debug() to get an idea of what the UI looks like at any point.
+  const handleSubmit = jest.fn()
   render(<Login onSubmit={handleSubmit} />)
-  // screen.debug() to get an idea of what the UI looks like at any point.
-  const username = 'chucknorris'
-  const password = 'i need no password'
+  const {username, password} = buildLoginForm()
 
   userEvent.type(screen.getByLabelText(/username/i), username)
   userEvent.type(screen.getByLabelText(/password/i), password)
   userEvent.click(screen.getByRole('button', {name: /submit/i}))
 
-  expect(submittedData).toEqual({
+  expect(handleSubmit).toHaveBeenCalledWith({
     username,
     password,
   })
+  expect(handleSubmit).toHaveBeenCalledTimes(1)
 })
-
 /*
 eslint
   no-unused-vars: "off",
