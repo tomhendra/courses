b4f19f0a3ef83070afb09682352ffcba8f0401ae
Tom Hendra <tom.hendra@outlook.com>
2021-10-28 20:04:03 +0200

    rust: the book chapter 1

---
+++ 39-the-rust-programming-language/hello_cargo/target/debug/deps/hello_cargo-06faf4398d4eb741.d
@@ -0,0 +1,5 @@
+/Users/tom/Dev/coursework/39-the-rust-programming-language/hello_cargo/target/debug/deps/hello_cargo-06faf4398d4eb741: src/main.rs
+
+/Users/tom/Dev/coursework/39-the-rust-programming-language/hello_cargo/target/debug/deps/hello_cargo-06faf4398d4eb741.d: src/main.rs
+
+src/main.rs:
---
+++ 39-the-rust-programming-language/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/tom/Dev/coursework/39-the-rust-programming-language/hello_cargo/target/debug/hello_cargo: /Users/tom/Dev/coursework/39-the-rust-programming-language/hello_cargo/src/main.rs
