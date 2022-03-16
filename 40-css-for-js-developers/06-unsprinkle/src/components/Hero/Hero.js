fb45c1667cbc2a8b44076b6a3772848891152744
Tom Hendra <tom.hendra@outlook.com>
2022-03-16 12:43:56 +0100

    css-for-js: module 6 workshop solution

--- 40-css-for-js-developers/06-unsprinkle/src/components/Hero/Hero.js
+++ 40-css-for-js-developers/06-unsprinkle/src/components/Hero/Hero.js
@@ -4,8 +4,29 @@ import styled from 'styled-components/macro';
 const Hero = () => {
   return (
     <Wrapper>
-      <HeroImage src="/images/hero-img.jpg" />
-      <Swoop src="/swoop.svg" />
+      <picture>
+        <source
+          type="image/avif"
+          srcSet="
+            /images/hero-img.avif 1x, 
+            /images/hero-img@2x.avif 2x, 
+            /images/hero-img@3x.avif 3x
+          "
+        />
+        <source
+          type="image/jpg"
+          srcSet="
+            /images/hero-img.jpg 1x, 
+            /images/hero-img@2x.jpg 2x, 
+            /images/hero-img@3x.jpg 3x
+          "
+        />
+        <HeroImage
+          src="/images/hero-img.jpg"
+          alt="an example of a photo on this site"
+        />
+      </picture>
+      <Swoop src="/swoop.svg" alt="" />
     </Wrapper>
   );
 };
